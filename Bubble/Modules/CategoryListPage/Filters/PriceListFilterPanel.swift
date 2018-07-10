4e02b0d90ada31ac7459adfddfc37307ab29713e
linlin.leo <linlin.leo@bytedance.com>
2018-07-10 20:31:38 +0800

    增加条件输入模块
    Change-Id: Ic24f5d48733a5fe1a4fed5c59bc70098ddc97e42

--- Bubble/Modules/CategoryListPage/Filters/PriceListFilterPanel.swift
+++ Bubble/Modules/CategoryListPage/Filters/PriceListFilterPanel.swift
@@ -56,6 +56,11 @@ class PriceListFilterPanel: UIView {
         return result
     }()
 
+    fileprivate lazy var bottomInputView: PriceBottomInputView = {
+        let re = PriceBottomInputView()
+        return re
+    }()
+
     override init(frame: CGRect) {
         let dataSource = PriceListTableViewDataSource()
         self.dataSource = dataSource
@@ -75,9 +80,17 @@ class PriceListFilterPanel: UIView {
         dataSource.didSelect = { [weak self] nodes in
             self?.didSelect?(nodes)
         }
+        
         addSubview(tableView)
+        addSubview(bottomInputView)
+        bottomInputView.snp.makeConstraints { maker in
+            maker.left.right.bottom.equalToSuperview()
+        }
+        bottomInputView.lu.addTopBorder()
+
         tableView.snp.makeConstraints { maker in
-            maker.top.left.right.bottom.equalToSuperview()
+            maker.top.left.right.equalToSuperview()
+            maker.bottom.equalTo(bottomInputView.snp.top).offset(0.5)
         }
         tableView.register(PriceListItemCell.self, forCellReuseIdentifier: "item")
         tableView.dataSource = dataSource
@@ -97,6 +110,8 @@ class PriceListTableViewDataSource: NSObject, UITableViewDataSource, UITableView
     var nodes: [Node] = []
 
     var selectedIndexPath: IndexPath?
+
+    var selectedIndexPaths: [IndexPath] = []
 
     var didSelect: (([Node]) -> Void)?
 
@@ -128,13 +143,28 @@ class PriceListItemCell: UITableViewCell {
         return result
     }()
 
+    lazy var checkboxBtn: UIButton = {
+        let re = UIButton()
+        re.setBackgroundImage(#imageLiteral(resourceName: "invalid-name"), for: .normal)
+        re.setBackgroundImage(#imageLiteral(resourceName: "invalid-name-checked"), for: .selected)
+        return re
+    }()
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(checkboxBtn)
+        checkboxBtn.snp.makeConstraints { maker in
+            maker.right.equalTo(-24)
+            maker.centerY.equalToSuperview()
+            maker.width.height.equalTo(14)
+        }
+
         contentView.addSubview(label)
 
         label.snp.makeConstraints { maker in
             maker.left.equalTo(24)
-            maker.right.equalToSuperview().offset(-24)
+            maker.right.equalTo(checkboxBtn.snp.left).offset(-24)
             maker.height.equalTo(21)
             maker.top.equalToSuperview().offset(12)
             maker.bottom.equalToSuperview().offset(-12)
@@ -147,4 +177,95 @@ class PriceListItemCell: UITableViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+fileprivate class PriceBottomInputView: UIView {
+
+    lazy var lowerPriceTextField: UITextField = {
+        let re = UITextField()
+        re.placeholder = "最低价格 (万)"
+        re.textAlignment = .center
+        re.backgroundColor = hexStringToUIColor(hex: "#f4f5f6")
+        re.font = CommonUIStyle.Font.pingFangRegular(12)
+        re.layer.cornerRadius = 4
+        return re
+    }()
+
+    lazy var upperPriceTextField: UITextField = {
+        let re = UITextField()
+        re.placeholder = "最高价格 (万)"
+        re.backgroundColor = hexStringToUIColor(hex: "#f4f5f6")
+        re.font = CommonUIStyle.Font.pingFangRegular(12)
+        re.textAlignment = .center
+        re.layer.cornerRadius = 4
+        return re
+    }()
+
+    lazy var configBtn: UIButton = {
+        let re = UIButton()
+        re.setTitle("确定", for: .normal)
+        re.setTitleColor(UIColor.white, for: .normal)
+        re.backgroundColor = hexStringToUIColor(hex: "#f85959")
+        return re
+    }()
+
+    lazy var seperaterLineView: UIView = {
+        let re = UIView()
+        re.backgroundColor = hexStringToUIColor(hex: "#d8d8d8")
+        return re
+    }()
+
+    lazy var topBorderLine: UIView = {
+        let re = UIView()
+        re.backgroundColor = hexStringToUIColor(hex: "#e8e8e8")
+        return re
+    }()
+
+    init() {
+        super.init(frame: CGRect.zero)
+
+        self.backgroundColor = UIColor.white
+        addSubview(lowerPriceTextField)
+        lowerPriceTextField.snp.makeConstraints { maker in
+            maker.left.equalTo(24)
+            maker.top.equalTo(8)
+            maker.bottom.equalTo(-8)
+            maker.height.equalTo(28)
+            maker.width.equalTo(105)
+        }
+
+        addSubview(seperaterLineView)
+        seperaterLineView.snp.makeConstraints { maker in
+            maker.left.equalTo(lowerPriceTextField.snp.right).offset(5)
+            maker.centerY.equalToSuperview()
+            maker.height.equalTo(1)
+            maker.width.equalTo(10)
+        }
+
+        addSubview(upperPriceTextField)
+        upperPriceTextField.snp.makeConstraints { maker in
+            maker.left.equalTo(seperaterLineView.snp.right).offset(5)
+            maker.top.equalTo(8)
+            maker.bottom.equalTo(-8)
+            maker.height.equalTo(28)
+            maker.width.equalTo(105)
+        }
+
+        addSubview(configBtn)
+        configBtn.snp.makeConstraints { maker in
+            maker.right.top.bottom.equalToSuperview()
+            maker.left.equalTo(upperPriceTextField.snp.right).offset(21)
+        }
+
+        addSubview(topBorderLine)
+        topBorderLine.snp.makeConstraints { maker in
+            maker.left.right.top.equalToSuperview()
+            maker.height.equalTo(0.5)
+         }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
