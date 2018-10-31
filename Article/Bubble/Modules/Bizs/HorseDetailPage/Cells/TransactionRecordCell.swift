db85231149155937f0073133c557d8391fec8519
xiefei.xf <xiefei.xf@bytedance.com>
2018-10-31 14:44:37 +0800

    修改小区成交历史间距

--- Article/Bubble/Modules/Bizs/HorseDetailPage/Cells/TransactionRecordCell.swift
+++ Article/Bubble/Modules/Bizs/HorseDetailPage/Cells/TransactionRecordCell.swift
@@ -55,15 +55,15 @@ class TransactionRecordCell: BaseUITableViewCell {
 
         contentView.addSubview(totalPriceLabel)
         totalPriceLabel.snp.makeConstraints { maker in
-            maker.right.equalTo(-15)
-            maker.top.equalTo(15)
+            maker.right.equalTo(-20)
+            maker.top.equalTo(18)
             maker.width.greaterThanOrEqualTo(45)
             maker.height.equalTo(22)
         }
 
         contentView.addSubview(namelabel)
         namelabel.snp.makeConstraints { maker in
-            maker.left.equalTo(15)
+            maker.left.equalTo(20)
             maker.top.equalTo(14)
             maker.right.equalTo(totalPriceLabel.snp.left).offset(-5)
             maker.height.equalTo(22)
@@ -71,15 +71,15 @@ class TransactionRecordCell: BaseUITableViewCell {
 
         contentView.addSubview(pricePreSqmLabel)
         pricePreSqmLabel.snp.makeConstraints { maker in
-            maker.right.equalTo(-15)
-            maker.height.equalTo(15)
+            maker.right.equalTo(-20)
+            maker.height.equalTo(20)
             maker.top.equalTo(totalPriceLabel.snp.bottom).offset(5)
         }
 
         contentView.addSubview(descLabel)
         descLabel.snp.makeConstraints { maker in
             maker.top.equalTo(namelabel.snp.bottom).offset(5)
-            maker.left.equalTo(15)
+            maker.left.equalTo(20)
             maker.bottom.equalToSuperview().offset(-13)
             maker.right.equalTo(pricePreSqmLabel.snp.left).offset(-5)
         }
@@ -88,8 +88,8 @@ class TransactionRecordCell: BaseUITableViewCell {
         bottomLine.snp.makeConstraints { maker in
             maker.height.equalTo(0.5)
             maker.bottom.equalToSuperview()
-            maker.left.equalToSuperview().offset(15)
-            maker.right.equalToSuperview().offset(-15)
+            maker.left.equalToSuperview().offset(20)
+            maker.right.equalToSuperview().offset(-20)
         }
     }
     
