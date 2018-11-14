bff9fb9d3d7ac90d12afdcfe1814695a7e0d8a95
“zhangjing” <zhangjing.2018@bytedance.com>
2018-11-14 21:12:42 +0800

    fix 代码冲突引起的购房小建议不显示

--- Article/Bubble/Modules/Bizs/HorseDetailPage/Cells/FHDetailSuggestTipCell.swift
+++ Article/Bubble/Modules/Bizs/HorseDetailPage/Cells/FHDetailSuggestTipCell.swift
@@ -143,28 +143,7 @@ func parsePriceRangeNode(_ priceRank: HousePriceRank?, traceExtension: TracerPar
         
         var buySuggestion: HousePriceRankSuggestion?
         if let thePriceRank = priceRank {
-            
-            guard let buySuggestion = thePriceRank.buySuggestion else {
-                
-                return nil
-            }
-            let cellRender = oneTimeRender(curry(fillPriceRangeCell)(buySuggestion))
-  
-            let params = EnvContext.shared.homePageParams <|>
-                toTracerParams("trade_tips", key: "element_type") <|>
-                toTracerParams("old_detail", key: "page_type") <|>
-            traceExtension
-            
-            return TableSectionNode(
-                items: [cellRender],
-                selectors: nil,
-                tracer: [elementShowOnceRecord(params: params)],
-                label: "",
-                type: .node(identifier: FHDetailSuggestTipCell.identifier))
-            
-        }else {
-            
-            return nil
+            buySuggestion = thePriceRank.buySuggestion
         }
         
         var suggestion: HousePriceRankSuggestion = HousePriceRankSuggestion()
@@ -172,8 +151,8 @@ func parsePriceRangeNode(_ priceRank: HousePriceRank?, traceExtension: TracerPar
         suggestion.content = "该房源当前价格合理，房东诚心出售，建议尽快联系经纪人看房"
         
         let cellRender = curry(fillPriceRangeCell)(buySuggestion ?? suggestion)
-        let params = TracerParams.momoid() <|>
-            toTracerParams("price_reference", key: "element_type") <|>
+        let params = EnvContext.shared.homePageParams <|>
+            toTracerParams("trade_tips", key: "element_type") <|>
             toTracerParams("old_detail", key: "page_type") <|>
         traceExtension
         
