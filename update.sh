c6814a1dc905e23eb21830d4362700770d13c6de
guchunhui <guchunhui@bytedance.com>
2018-11-23 14:46:49 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 echo "========will update========="
 
-git submodule foreach git checkout feauture/develop
+git submodule foreach git checkout feature/develop
 git submodule foreach git pull origin feature/develop
 
 echo "========update done========="
