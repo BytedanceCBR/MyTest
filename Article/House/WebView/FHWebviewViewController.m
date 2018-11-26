ab2a90a8abde82b1851ebaf51aeabc4ff53efa98
zhangyuanke <zhangyuanke@bytedance.com>
2018-11-26 21:50:07 +0800

    返回键以及title基本ok

--- Article/House/WebView/FHWebviewViewController.m
+++ Article/House/WebView/FHWebviewViewController.m
@@ -11,6 +11,8 @@
 #import <TTRStaticPlugin.h>
 #import "UIViewController+NavbarItem.h"
 #import "TTRoute.h"
+#import "TTRWebViewProgressView.h"
+#import "SSNavigationBar.h"
 
 @interface FHWebviewViewController ()<TTRouteInitializeProtocol>
 
@@ -22,13 +24,31 @@
 
 -(void)initNavbar
 {
-    UIBarButtonItem *backItem = [self defaultBackItemWithTarget:self action:@selector(backAction)];
+//    UIImage *img = [UIImage imageNamed:@"icon-return"];
+//    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//    [backButton setImage:img forState:UIControlStateNormal];
+//    [backButton setImage:img forState:UIControlStateHighlighted];
+//    backButton.frame = CGRectMake(0, 0, 44, 44);
+//    [backButton addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    
+//    UIImage *image = [UIImage imageNamed:@"icon-return"];
+//    if (target == nil) {
+//        target = self;
+//    }
+//    if (selector == nil) {
+//        selector = @selector(_defaultBackAction);
+//    }
+    
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:[SSNavigationBar navigationBackButtonWithTarget:self action:@selector(backAction)]];
+    
+//    UIBarButtonItem *backItem = [self defaultBackItemWithTarget:self action:@selector(backAction)];
+//    backItem.image = img;
     self.navigationItem.leftBarButtonItem = backItem;
     
     UILabel *label = [self defaultTitleView];
-    label.text = @"测试页面";
+    label.text = @"123Title";
     [label sizeToFit];
-    
+
     self.navigationItem.titleView = label;
 }
 
@@ -55,7 +75,8 @@
         NSArray *history = [userInfo.allInfo objectForKey:@"history"];
         
         
-        
+
+//        self.url = @"https://www.baidu.com";
         self.url = url;
         self.dic = [NSMutableDictionary dictionary];
         
@@ -69,23 +90,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    if (@available(iOS 11.0 , *)) {
+        self.webview.ttr_scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        UIEdgeInsets inset = UIEdgeInsetsZero;
+        inset.bottom = [[UIApplication sharedApplication] keyWindow].safeAreaInsets.bottom;
+        self.webview.ttr_scrollView.contentInset = inset;
+    }
+    
+    self.webview.frame = CGRectMake(0, 44.f + self.view.tt_safeAreaInsets.top, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height - (44.f + self.view.tt_safeAreaInsets.top));
+//    if #available(iOS 11.0, *) {
+//        tableView.contentInsetAdjustmentBehavior = .never
+//    }
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
     [self initNavbar];
     self.viewModel = [[FHWebviewViewModel alloc] initWithViewController:self];
     [self.viewModel registerJSBridge:self.webview.ttr_staticPlugin];
-    
-//    __weak typeof(self) wself = self;
-//    self.viewModel.identifyFinishBlock = ^(NSDictionary *info) {
-//        if (wself.identifyDoneFinishBlock) {
-//            wself.identifyDoneFinishBlock();
-//        }
-//        [wself.navigationController popViewControllerAnimated:YES];
-//    };
-    
 
     
     NSURL *u = [NSURL URLWithString:self.url];
     NSURLRequest *request = [[NSURLRequest alloc]initWithURL:u];
     [self.webview ttr_loadRequest:request];
+    
+//    TTRWebViewProgressView *progressView = self.progressView;
     
     
    
@@ -106,5 +134,8 @@
 //        
 //    }];
 }
+
+#pragma mark - TTRWebViewDelegate
+
 
 @end
