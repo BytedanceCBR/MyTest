98df120305efa37ab4b6304250b662ffcc9a0424
“zhangjing” <zhangjing.2018@bytedance.com>
2018-11-27 22:25:54 +0800

    enter_mapfind埋点完成，进入地图参数传递

--- Article/House/HouseHome/FHHomeCellHelper.m
+++ Article/House/HouseHome/FHHomeCellHelper.m
@@ -436,12 +436,26 @@ static NSMutableArray  * _Nullable identifierArr;
         [contextBridge setTraceValue:@"city_market" forKey:@"origin_from"];
         [contextBridge setTraceValue:@"be_null" forKey:@"origin_search_id"];
         
-        if (model.mapOpenUrl) {
+        if (model.mapOpenUrl.length > 0) {
             
-            NSURL *url = [NSURL URLWithString:model.mapOpenUrl];
+            NSMutableString *urlStr = [NSMutableString stringWithString:model.mapOpenUrl];
+            if (![urlStr containsString:@"enter_from"]) {
+                [urlStr appendString:@"&enter_from=city_market"];
+            }
+            if (![urlStr containsString:@"search_id"]) {
+                [urlStr appendString:@"&search_id=be_null"];
+            }
+            if (![urlStr containsString:@"origin_from"]) {
+                [urlStr appendString:@"&origin_from=city_market"];
+            }
+            if (![urlStr containsString:@"origin_search_id"]) {
+                [urlStr appendString:@"&origin_search_id=be_null"];
+            }
+            NSURL *url = [NSURL URLWithString:urlStr];
             [[TTRoute sharedRoute] openURLByPushViewController:url userInfo:nil];
         }
         [wself addHomeCityMarketClickLog];
+        [wself addCityMarketEnterMapFindLog];
     };
 }
 
@@ -513,6 +527,18 @@ static NSMutableArray  * _Nullable identifierArr;
     param[@"page_type"] = @"maintab";
     [FHUserTracker writeEvent:@"city_market_click" params:param];
 }
+
++(void)addCityMarketEnterMapFindLog
+{
+    NSMutableDictionary *param = [NSMutableDictionary dictionary];
+    param[@"enter_from"] = @"city_market";
+    param[@"search_id"] = @"be_null";
+    param[@"origin_from"] = @"city_market";
+    param[@"origin_search_id"] = @"be_null";
+
+    [FHUserTracker writeEvent:@"enter_mapfind" params:param];
+}
+
 
 @end
 
