4d61fe7bf575e51ba802b1fcc7c27fdd603fba6d
xiefei.xf <xiefei.xf@bytedance.com>
2018-12-18 10:48:59 +0800

    修改头像不显示问题

--- AKPlugins/TTAvatar/Classes/Avatar/ExploreAvatarView.m
+++ AKPlugins/TTAvatar/Classes/Avatar/ExploreAvatarView.m
@@ -30,7 +30,6 @@
         self.imageView = [[TTImageView alloc] initWithFrame:self.bounds];
         self.imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         [self addSubview:self.imageView];
-        self.imageView.hidden = YES;
         
         self.highlightedMaskView = [[SSThemedView alloc] initWithFrame:self.bounds];
         self.highlightedMaskView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
