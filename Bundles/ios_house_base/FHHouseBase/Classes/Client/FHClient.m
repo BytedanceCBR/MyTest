59dfc50080e31a9b40074c38f29e4edf11194915
xiefei.xf <xiefei.xf@bytedance.com>
2018-12-20 15:31:27 +0800

    增加Client通用类

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Client/FHClient.m
@@ -0,0 +1,12 @@
+//
+//  FHClient.m
+//  AFgzipRequestSerializer
+//
+//  Created by 谢飞 on 2018/12/20.
+//
+
+#import "FHClient.h"
+
+@implementation FHClient
+
+@end
---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Client/FHEnvContext.m
@@ -0,0 +1,12 @@
+//
+//  FHEnvContext.m
+//  AFgzipRequestSerializer
+//
+//  Created by 谢飞 on 2018/12/20.
+//
+
+#import "FHEnvContext.h"
+
+@implementation FHEnvContext
+
+@end
