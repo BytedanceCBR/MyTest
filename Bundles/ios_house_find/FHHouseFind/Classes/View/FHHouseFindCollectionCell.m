badf7c74a59946a21c93ab3952b04ad835e9ec1f
“zhangjing” <zhangjing.2018@bytedance.com>
2019-01-04 16:23:06 +0800

    找房tab UI搭建，剩个小问题

--- Bundles/ios_house_find/FHHouseFind/Classes/View/FHHouseFindCollectionCell.m
+++ Bundles/ios_house_find/FHHouseFind/Classes/View/FHHouseFindCollectionCell.m
@@ -6,7 +6,45 @@
 //
 
 #import "FHHouseFindCollectionCell.h"
+#import "FHHouseFindListView.h"
+#import "Masonry.h"
+
+@interface FHHouseFindCollectionCell ()
+
+@property(nonatomic , strong) FHHouseFindListView *containerView;
+
+@end
 
 @implementation FHHouseFindCollectionCell
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        [self setupUI];
+    }
+    return self;
+}
+- (void)updateDataWithOpenUrl:(NSString *)openUrl
+{
+    [self.containerView updateDataWithOpenUrl:openUrl];
+}
+- (void)setupUI
+{
+    [self.contentView addSubview:self.containerView];
+    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+         
+        make.left.right.bottom.mas_equalTo(self.contentView);
+        make.top.mas_equalTo(self.contentView);
+     }];
+}
+
+- (FHHouseFindListView *)containerView
+{
+    if (!_containerView) {
+        _containerView = [[FHHouseFindListView alloc]initWithFrame:CGRectZero];
+    }
+    return _containerView;
+}
 @end
