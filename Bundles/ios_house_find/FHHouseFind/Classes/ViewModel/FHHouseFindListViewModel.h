e3f7bda6532dafb30dd6b3be0b814da41cfb5b15
“zhangjing” <zhangjing.2018@bytedance.com>
2019-01-07 11:57:22 +0800

    找房tab异常处理

--- Bundles/ios_house_find/FHHouseFind/Classes/ViewModel/FHHouseFindListViewModel.h
+++ Bundles/ios_house_find/FHHouseFind/Classes/ViewModel/FHHouseFindListViewModel.h
@@ -11,15 +11,16 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class HMSegmentedControl;
+@class HMSegmentedControl,FHHouseFindListViewController,FHErrorView;
 @interface FHHouseFindListViewModel : NSObject
 
-- (instancetype)initWithScrollView:(UIScrollView *)scrollView;
+- (instancetype)initWithScrollView:(UIScrollView *)scrollView viewController:(FHHouseFindListViewController *)listVC;
 - (void)jump2GuessVC;
 - (void)setTracerModel:(FHTracerModel *)tracerModel;
 - (void)setSegmentView:(HMSegmentedControl *)segmentView;
 - (void)addConfigObserver;
 - (void)viewDidLayoutSubviews;
+- (void)setErrorMaskView:(FHErrorView *)errorMaskView;
 
 @end
 
