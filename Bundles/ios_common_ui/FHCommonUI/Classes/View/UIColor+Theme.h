598c0b48420629841f786e4097192da00710699f
guchunhui <guchunhui@bytedance.com>
2019-01-07 20:04:57 +0800

    color+theme添加宏

--- Bundles/ios_common_ui/FHCommonUI/Classes/View/UIColor+Theme.h
+++ Bundles/ios_common_ui/FHCommonUI/Classes/View/UIColor+Theme.h
@@ -9,7 +9,8 @@
 
 #define RGB(r,g,b)    [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
-
+#define HEXRGBA(str)   [UIColor colorWithHexString:str]
+#define HEXINTRGB(hex) [UIColor colorWithRGB:hex]
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIColor (Theme)
