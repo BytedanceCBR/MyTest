9f6c4e729ce9d3f997231aaa2c12e1310b1db3da
zhangyuanke <zhangyuanke@bytedance.com>
2019-01-08 15:57:19 +0800

    城市列表cell 创建

---
+++ Bundles/ios_house_home/FHHouseHome/Classes/CityListVC/CitySearch/View/FHCitySearchItemCell.h
@@ -0,0 +1,16 @@
+//
+//  FHCitySearchItemCell.h
+//  FHHouseHome
+//
+//  Created by 张元科 on 2019/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHCitySearchItemCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_home/FHHouseHome/Classes/CityListVC/CitySearch/View/FHCitySearchItemCell.m
@@ -0,0 +1,23 @@
+//
+//  FHCitySearchItemCell.m
+//  FHHouseHome
+//
+//  Created by 张元科 on 2019/1/8.
+//
+
+#import "FHCitySearchItemCell.h"
+
+@implementation FHCitySearchItemCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
