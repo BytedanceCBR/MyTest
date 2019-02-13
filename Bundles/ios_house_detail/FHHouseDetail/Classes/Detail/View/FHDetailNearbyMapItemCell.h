fefac91a84c3a952b816e7ef69d94f735c0b24e5
xiefei.xf <xiefei.xf@bytedance.com>
2019-02-13 16:13:17 +0800

    [update]:修改地图cell

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/FHDetailNearbyMapItemCell.h
@@ -0,0 +1,18 @@
+//
+//  FHDetailNearbyMapItemCell.h
+//  AFgzipRequestSerializer
+//
+//  Created by 谢飞 on 2019/2/12.
+//
+
+#import "FHDetailBaseCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHDetailNearbyMapItemCell : FHDetailBaseCell
+
+- (void)updateText:(NSString *)name andDistance:(NSString *)distance;
+
+@end
+
+NS_ASSUME_NONNULL_END
