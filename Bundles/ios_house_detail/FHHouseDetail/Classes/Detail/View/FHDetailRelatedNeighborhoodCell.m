cf4b4f9fa969fed4acff53432d51265c40ccf8a1
zhangyuanke <zhangyuanke@bytedance.com>
2019-02-15 18:44:37 +0800

    二手房周边小区cell构建基本ok

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/FHDetailRelatedNeighborhoodCell.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/FHDetailRelatedNeighborhoodCell.m
@@ -48,6 +48,8 @@
     //
     FHDetailRelatedNeighborhoodModel *model = (FHDetailRelatedNeighborhoodModel *)data;
     if (model.relatedNeighborhoodData) {
+        self.headerView.label.text = [NSString stringWithFormat:@"周边小区(%@)",model.relatedNeighborhoodData.total];
+        self.headerView.isShowLoadMore = model.relatedNeighborhoodData.hasMore;
         UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
         flowLayout.sectionInset = UIEdgeInsetsMake(0, 20, 0, 20);
         flowLayout.itemSize = CGSizeMake(156, 170);
@@ -86,7 +88,7 @@
         make.left.top.right.mas_equalTo(self.contentView);
         make.height.mas_equalTo(46);
     }];
-    
+    [self.headerView addTarget:self action:@selector(moreButtonClick:) forControlEvents:UIControlEventTouchUpInside];
     _containerView = [[UIView alloc] init];
     _containerView.clipsToBounds = YES;
     _containerView.backgroundColor = [UIColor whiteColor];
@@ -96,6 +98,14 @@
         make.left.right.mas_equalTo(self.contentView);
         make.bottom.mas_equalTo(self.contentView).offset(-20);
     }];
+}
+
+// 查看更多
+- (void)moreButtonClick:(UIButton *)button {
+    FHDetailRelatedNeighborhoodModel *model = (FHDetailRelatedNeighborhoodModel *)self.currentData;
+    if (model.relatedNeighborhoodData && model.relatedNeighborhoodData.hasMore) {
+        // 点击事件处理
+    }
 }
 
 @end
@@ -111,9 +121,90 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor grayColor];
+        self.backgroundColor = [UIColor whiteColor];
+        [self setupUI];
     }
     return self;
+}
+
+- (void)refreshWithData:(id)data {
+    if (self.currentData == data || ![data isKindOfClass:[FHDetailRelatedNeighborhoodResponseDataItemsModel class]]) {
+        return;
+    }
+    self.currentData = data;
+    FHDetailRelatedNeighborhoodResponseDataItemsModel *model = (FHDetailRelatedNeighborhoodResponseDataItemsModel *)data;
+    if (model) {
+        if (model.images.count > 0) {
+            FHDetailRelatedNeighborhoodResponseDataItemsImagesModel *imageModel = model.images[0];
+            NSString *urlStr = imageModel.url;
+            if ([urlStr length] > 0) {
+                [self.icon bd_setImageWithURL:[NSURL URLWithString:urlStr] placeholder:[UIImage imageNamed:@"default_image"]];
+            } else {
+                self.icon.image = [UIImage imageNamed:@"default_image"];
+            }
+        } else {
+            self.icon.image = [UIImage imageNamed:@"default_image"];
+        }
+        self.descLabel.text = model.displayTitle;
+        self.priceLabel.text = model.displayPricePerSqm;
+        self.spaceLabel.text = model.displayBuiltYear;
+    }
+    [self layoutIfNeeded];
+}
+
+- (void)setupUI {
+    _icon = [[UIImageView alloc] init];
+    _icon.layer.cornerRadius = 4.0;
+    _icon.layer.masksToBounds = YES;
+    _icon.layer.borderWidth = 0.5;
+    _icon.layer.borderColor = [[UIColor colorWithHexString:@"#e8eaeb"] CGColor];
+    [self addSubview:_icon];
+    
+    _descLabel = [UILabel createLabel:@"" textColor:@"#081f33" fontSize:16];
+    [self addSubview:_descLabel];
+    
+    _priceLabel = [UILabel createLabel:@"" textColor:@"#f85959" fontSize:16];
+    [self addSubview:_priceLabel];
+    
+    _spaceLabel = [UILabel createLabel:@"" textColor:@"#ffffff" fontSize:12];
+    [self addSubview:_spaceLabel];
+    
+    [self.icon mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_equalTo(self);
+        make.width.mas_equalTo(156);
+        make.height.mas_equalTo(116);
+        make.top.mas_equalTo(self);
+    }];
+    
+    UIColor *topColor = RGBA(255, 255, 255, 0);
+    UIColor *bottomColor = RGBA(0, 0, 0, 0.5);
+    NSArray *gradientColors = [NSArray arrayWithObjects:(id)(topColor.CGColor), (id)(bottomColor.CGColor), nil];
+    NSArray *gradientLocations = @[@(0),@(1)];
+    CAGradientLayer *gradientlayer = [[CAGradientLayer alloc] init];
+    gradientlayer.colors = gradientColors;
+    gradientlayer.locations = gradientLocations;
+    gradientlayer.frame = CGRectMake(0, 0, 156, 120);
+    gradientlayer.cornerRadius = 4.0;
+    [self.icon.layer addSublayer:gradientlayer];
+    
+    [self.descLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_equalTo(self);
+        make.height.mas_equalTo(22);
+        make.top.mas_equalTo(self.icon.mas_bottom).offset(10);
+    }];
+    
+    [self.spaceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(17);
+        make.right.mas_equalTo(self).offset(-6);
+        make.bottom.mas_equalTo(self.icon.mas_bottom).offset(-6);
+    }];
+    
+    [self.priceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_equalTo(self);
+        make.height.mas_equalTo(22);
+        make.top.mas_equalTo(self.descLabel.mas_bottom);
+        make.bottom.mas_equalTo(self);
+    }];
 }
 
 @end
