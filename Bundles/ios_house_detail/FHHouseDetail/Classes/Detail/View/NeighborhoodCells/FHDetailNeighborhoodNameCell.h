c4009da9096159c9eb0094ac6c4516ff2a1ddaaf
zhangyuanke <zhangyuanke@bytedance.com>
2019-02-18 16:50:16 +0800

    小区详情页开始画ui

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/NeighborhoodCells/FHDetailNeighborhoodNameCell.h
@@ -0,0 +1,24 @@
+//
+//  FHDetailNeighborhoodNameCell.h
+//  FHHouseDetail
+//
+//  Created by 张元科 on 2019/2/18.
+//
+
+#import "FHDetailBaseCell.h"
+#import "FHDetailBaseModel.h"
+#import "FHDetailNeighborhoodModel.h"
+#import "FHHouseDetailBaseViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHDetailNeighborhoodNameCell : FHDetailBaseCell
+
+@end
+
+//FHDetailNeighborhoodNameModel
+@interface FHDetailNeighborhoodNameModel : FHDetailBaseModel
+
+@end
+
+NS_ASSUME_NONNULL_END
