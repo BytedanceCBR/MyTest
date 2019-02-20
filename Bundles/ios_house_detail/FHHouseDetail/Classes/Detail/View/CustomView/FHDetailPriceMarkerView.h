27a6d133aaf81977e768607141427838769b3f9f
“zhangjing” <zhangjing.2018@bytedance.com>
2019-02-20 15:54:28 +0800

    [UPDATE]图表cell初步UI

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/CustomView/FHDetailPriceMarkerView.h
@@ -0,0 +1,40 @@
+//
+//  FHDetailPriceMarkerView.h
+//  PNChartDemo
+//
+//  Created by 张静 on 2019/2/19.
+//  Copyright © 2019年 kevinzhow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FHDetailBaseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class FHDetailPriceMarkerData,FHDetailPriceMarkerItem;
+
+//typedef (CGFloat,FHDetailPriceTrendValuesModel)(^markerDataBlock)(NSInteger index);
+
+@interface FHDetailPriceMarkerView : UIView
+
+@property(nonatomic, copy, nullable) FHDetailPriceMarkerData *markData;
+
+- (void)refreshContent:(FHDetailPriceMarkerData *)markData;
+
+@end
+
+@interface FHDetailPriceMarkerData : NSObject
+
+@property(nonatomic, assign) double unitPerSquare;
+@property(nonatomic, strong, nullable) NSArray<FHDetailPriceMarkerItem *> *trendItems;
+
+@end
+
+@interface FHDetailPriceMarkerItem : NSObject
+
+@property(nonatomic, copy, nullable) NSString *name;
+@property(nonatomic, strong, nullable) FHDetailPriceTrendValuesModel *priceModel;
+
+@end
+
+NS_ASSUME_NONNULL_END
