cfdd05a83080d6118b9adc7d131f4b6c3598d0ad
xiefei.xf <xiefei.xf@bytedance.com>
2019-03-22 16:41:09 +0800

    增加离线化开关

--- Bundles/ios_house_base/FHHouseBase/Classes/Client/FHIESGeckoManager.m
+++ Bundles/ios_house_base/FHHouseBase/Classes/Client/FHIESGeckoManager.m
@@ -10,13 +10,12 @@
 #import <TTInstallIDManager.h>
 #import <IESFalconManager.h>
 #import "SSZipArchive.h"
+#import "FHHouseBridgeManager.h"
 
 #if DEBUG
 static NSString * const kFHIESGeckoKey = @"adc27f2b35fb3337a4cb1ea86d05db7a";
-//static NSString * const kFHIESGeckoKey = @"7838c7618ea608a0f8ad6b04255b97b9";
 #else
-static NSString * const kFHIESGeckoKey = @"adc27f2b35fb3337a4cb1ea86d05db7a";
-//static NSString * const kFHIESGeckoKey = @"7838c7618ea608a0f8ad6b04255b97b9";
+static NSString * const kFHIESGeckoKey = @"7838c7618ea608a0f8ad6b04255b97b9";
 #endif
 
 @implementation FHIESGeckoManager
@@ -30,18 +29,13 @@ static NSString * const kFHIESGeckoKey = @"adc27f2b35fb3337a4cb1ea86d05db7a";
 
 + (void)configIESWebFalcon
 {
-    IESFalconManager.interceptionEnable = YES;
-    
-    /*
-    NSString *searchPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-    [SSZipArchive unzipFileAtPath:[[NSBundle mainBundle] pathForResource:@"falcon_dyfe" ofType:@"zip"] toDestin ation:searchPath];
-    [SSZipArchive unzipFileAtPath:[[NSBundle mainBundle] pathForResource:@"wallet" ofType:@"zip"] toDestination:searchPath];
-    */
-    
-    NSString *pattern = @"^(http|https)://.*.[bytecdn.cn|snssdk.com|pstatp.com]/toutiao/";
-    //[IESFalconManager registerPattern:pattern forSearchPath:searchPath];
-    [IESFalconManager registerPattern:pattern forGeckoAccessKey:kFHIESGeckoKey];
-//    [IESFalconManager registerPattern:@"https://webcast.amemv.com/falcon/" forGeckoAccessKey:@"2d15e0aa4fe4a5c91eb47210a6ddf467"];
+    if ([[[FHHouseBridgeManager sharedInstance] envContextBridge] isOpenWebOffline]) {
+        IESFalconManager.interceptionWKHttpScheme = YES;
+        IESFalconManager.interceptionEnable = YES;
+        
+        NSString *pattern = @"^(http|https)://.*.[pstatp.com]/toutiao/";
+        [IESFalconManager registerPattern:pattern forGeckoAccessKey:kFHIESGeckoKey];
+    }
 }
 
 @end
