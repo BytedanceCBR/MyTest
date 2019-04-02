e2738104bd691cbff3fe31b1d3c0555d7e6d1b2b
zhangyuanke <zhangyuanke@bytedance.com>
2019-04-02 14:01:55 +0800

    banner cell 高度计算

--- Bundles/ios_house_home/FHHouseHome/Classes/cells/FHHomeScrollBannerCell.h
+++ Bundles/ios_house_home/FHHouseHome/Classes/cells/FHHomeScrollBannerCell.h
@@ -18,4 +18,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+@interface FHHomeScrollBannerView : UIView
+
+@end
+
 NS_ASSUME_NONNULL_END
--- Bundles/ios_house_home/FHHouseHome/Classes/cells/FHHomeScrollBannerCell.m
+++ Bundles/ios_house_home/FHHouseHome/Classes/cells/FHHomeScrollBannerCell.m
@@ -7,18 +7,50 @@
 
 #import "FHHomeScrollBannerCell.h"
 
+static CGFloat kFHScrollBannerTopMargin = 10.5;
+static CGFloat kFHScrollBannerHeight = 57.0; // 轮播图的高度
+
+@interface FHHomeScrollBannerCell ()
+
+@property (nonatomic, strong)   FHHomeScrollBannerView       *bannerView;
+
+@end
+
 @implementation FHHomeScrollBannerCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-       
+        [FHHomeScrollBannerCell cellHeight];
+        [self setupUI];
     }
     return self;
 }
 
 + (CGFloat)cellHeight {
-    return 78;
+    kFHScrollBannerHeight = 57.0;
+    kFHScrollBannerHeight = ([UIScreen mainScreen].bounds.size.width - 40) / 335.0f * kFHScrollBannerHeight;
+    return kFHScrollBannerHeight + kFHScrollBannerTopMargin * 2;
 }
+
+- (void)setupUI {
+    _bannerView = [[FHHomeScrollBannerView alloc] init];
+    _bannerView.backgroundColor = [UIColor redColor];
+    [self.contentView addSubview:_bannerView];
+    [_bannerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(20);
+        make.right.mas_equalTo(-20);
+        make.bottom.mas_equalTo(self.contentView);// 下面的降价房cell之前布局有问题
+        make.height.mas_equalTo(kFHScrollBannerHeight);
+    }];
+}
+
+@end
+
+
+// FHHomeScrollBannerView
+
+@implementation FHHomeScrollBannerView
+
 
 @end
