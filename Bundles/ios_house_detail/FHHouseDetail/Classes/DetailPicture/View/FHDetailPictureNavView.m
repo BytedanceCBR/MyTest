a2f74fa4257a8fc5cf6849004c382f93e7cfc5e9
zhangyuanke <zhangyuanke@bytedance.com>
2019-04-15 21:03:07 +0800

    详情页分类图片添加VC

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/DetailPicture/View/FHDetailPictureNavView.m
@@ -0,0 +1,20 @@
+//
+//  FHDetailPictureNavView.m
+//  FHHouseDetail
+//
+//  Created by 张元科 on 2019/4/15.
+//
+
+#import "FHDetailPictureNavView.h"
+
+@implementation FHDetailPictureNavView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/DetailPicture/ViewModel/FHDetailPictureViewModel.m
@@ -0,0 +1,12 @@
+//
+//  FHDetailPictureViewModel.m
+//  FHHouseDetail
+//
+//  Created by 张元科 on 2019/4/15.
+//
+
+#import "FHDetailPictureViewModel.h"
+
+@implementation FHDetailPictureViewModel
+
+@end
