596c633b89c4af8cb39f3d14c755780d2849a859
xiefei.xf <xiefei.xf@bytedance.com>
2019-04-23 17:04:05 +0800

    修改组件名

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
@@ -534,7 +534,7 @@ typedef enum : NSUInteger {
     {
         dict[@"page_type"] = @"realtor_detail";
         
-        NSURL *openUrlRn = [NSURL URLWithString:[NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=0&report_params=%@&im_params=%@&bundle_name=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle"]];
+        NSURL *openUrlRn = [NSURL URLWithString:[NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule_home&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=0&report_params=%@&im_params=%@&bundle_name=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle"]];
         
         TTRouteUserInfo *userInfo = [[TTRouteUserInfo alloc]initWithInfo:info];
         //    [[TTRoute sharedRoute]openURLByViewController:openUrlRn userInfo:userInfo];
