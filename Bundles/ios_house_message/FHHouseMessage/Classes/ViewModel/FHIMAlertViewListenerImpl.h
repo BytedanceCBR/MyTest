f296f615865efb7466bf74170efd48a007af43de
linlin.leo <linlin.leo@bytedance.com>
2019-04-30 15:53:21 +0800

    [bigfix]头条业务下沉】视频全屏播放，收到B端给C端发送的消息，消息通知框展示不全，不点击消息时，点击屏幕，通知栏展示在横屏的左侧

---
+++ Bundles/ios_house_message/FHHouseMessage/Classes/ViewModel/FHIMAlertViewListenerImpl.h
@@ -0,0 +1,17 @@
+//
+//  FHIMAlertViewListenerImpl.h
+//  AKCommentPlugin
+//
+//  Created by leo on 2019/4/30.
+//
+
+#import <Foundation/Foundation.h>
+#import "IMManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHIMAlertViewListenerImpl : NSObject<FHIMAlertViewListener>
++(instancetype)shareInstance;
+@end
+
+NS_ASSUME_NONNULL_END
