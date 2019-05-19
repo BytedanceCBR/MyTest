4f404474328233b4133ac76b54496cf7d4915079
fupeidong <fupeidong.rd@bytedance.com>
2019-05-19 20:23:06 +0800

    [update]move im share to c client

---
+++ Bundles/ios_house_message/FHHouseMessage/Classes/share/view/FHIMShareUserCell.h
@@ -0,0 +1,17 @@
+//
+//  FHIMShareUserCell.h
+//  ios_house_im
+//
+//  Created by leo on 2019/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHIMShareUserCell : UITableViewCell
+@property (nonatomic, strong) UIImageView* avator;
+@property (nonatomic, strong) UILabel* title;
+@end
+
+NS_ASSUME_NONNULL_END
