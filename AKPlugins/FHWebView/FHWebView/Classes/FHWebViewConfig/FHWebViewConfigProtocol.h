94b2dbcb52899ef675c6df66ce2c75e884623829
xiesiming <xiesiming@bytedance.com>
2019-05-19 16:36:39 +0800

    加注释

--- AKPlugins/FHWebView/FHWebView/Classes/FHWebViewConfig/FHWebViewConfigProtocol.h
+++ AKPlugins/FHWebView/FHWebView/Classes/FHWebViewConfig/FHWebViewConfigProtocol.h
@@ -19,24 +19,25 @@ typedef NS_ENUM(NSInteger, FHAppVersion)
 
 @protocol FHWebViewConfigProtocol <NSObject>
 
+//客户端版本
 + (FHAppVersion)appVersion;
-
+//启动条颜色
 + (UIColor *)progressViewLineFillColor;
-
+//显示网络错误页
 - (void)showEmptyView:(UIView *)view retryBlock:(void (^)(void))retryBlock;
-
+//隐藏网络错误页
 - (void)hideEmptyView;
-
+//加载中
 - (void)showLoading:(UIView *)view;
-
+//停止加载
 - (void)hideLoading;
-//返回自定义导航栏标题label
+//返回自定义导航栏标题样式
 + (UILabel *)defaultTitleView;
-
+//返回请求的公共参数
 + (NSDictionary *)getRequestCommonParams;
-
+//登录取消时回调
 + (void)onAccountCancellationSuccessCallback:(TTRJSBResponse)callback controller:(UIViewController *)controller;
-
+//登录时调用
 + (void)loginWithParam:(NSDictionary *)param webView:(UIView<TTRexxarEngine> *)webview;
 
 @end
