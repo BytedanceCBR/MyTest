b5b61a133b9b7bb47f2a7d52fe981632c2e6b9e9
zhangyuanke <zhangyuanke@bytedance.com>
2019-05-29 12:54:18 +0800

    TTLocationManager 部分修改

--- TTBundles/ios_tt_articlebase/TTArticleBase/Classes/Common/SSLocationManager/TTLocationManager.h
+++ TTBundles/ios_tt_articlebase/TTArticleBase/Classes/Common/SSLocationManager/TTLocationManager.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 #import "TTPlacemarkItemProtocol.h"
+#import "TTPlacemarkItem.h"
 
 extern NSString *const TTLocationManagerCityDidChangedNotification;
 
@@ -33,16 +34,6 @@ typedef void(^TTGeocodeHandler) (id<TTGeocodeProtocol> geocoder, TTPlacemarkItem
 
 @end
 
-@interface TTPlacemarkItem : NSObject <TTPlacemarkItemProtocol>
-
-@property(nonatomic) CLLocationCoordinate2D coordinate;
-@property(nonatomic) NSTimeInterval  timestamp;
-@property(nonatomic, copy) NSString  *address;
-@property(nonatomic, copy) NSString  *province;
-@property(nonatomic, copy) NSString  *city;
-@property(nonatomic, copy) NSString  *district;
-
-@end
 
 typedef NS_ENUM(NSInteger, TTLocationCommandType) {
     TTLocationCommandTypeNone,
