848d93f26337c1ad3fd7e5730c5358e0e32b52f3
xiefei.xf <xiefei.xf@bytedance.com>
2019-05-30 10:50:37 +0800

    [update]:增加二手房虚假列表页

---
+++ Bundles/ios_house_list/FHHouseList/Classes/House/Controller/FHFalseHouseListViewController.m
@@ -0,0 +1,31 @@
+//
+//  FHFalseHouseListViewController.m
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2019/5/30.
+//
+
+#import "FHFalseHouseListViewController.h"
+
+@interface FHFalseHouseListViewController ()
+
+@end
+
+@implementation FHFalseHouseListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
