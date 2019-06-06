141a900e438651ddcbf750cdcbadedf4cf970fe6
xiefei.xf <xiefei.xf@bytedance.com>
2019-06-06 18:27:34 +0800

    [update]:修改ipod touch 崩溃问题

--- Bundles/ios_house_list/FHHouseList/Classes/House/Controller/FHHouseDetailWebViewController.h
+++ Bundles/ios_house_list/FHHouseList/Classes/House/Controller/FHHouseDetailWebViewController.h
@@ -6,10 +6,10 @@
 //
 
 #import "SSWebViewController.h"
-#import <FHBaseViewController.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FHHouseDetailWebViewController : FHBaseViewController
+@interface FHHouseDetailWebViewController : SSWebViewController
 
 @end
 
--- Bundles/ios_house_list/FHHouseList/Classes/House/Controller/FHHouseDetailWebViewController.m
+++ Bundles/ios_house_list/FHHouseList/Classes/House/Controller/FHHouseDetailWebViewController.m
@@ -26,51 +26,50 @@
 #import "UIView+Refresh_ErrorHandler.h"
 #import <FHUtils.h>
 #import <FHMainApi.h>
+#import <UIFont+House.h>
+#import <UIColor+Theme.h>
 
 @interface FHHouseDetailWebViewController ()
 {
-    NSTimeInterval _startTime;
+    
 }
 
-@property (nonatomic, strong) SSWebViewContainer *webContainer;
-@property (nonatomic, strong) TTRouteUserInfo *realtorUserInfo;
-@property (nonatomic, strong) FHHouseDetailPhoneCallViewModel *phoneCallViewModel;
-@property (nonatomic, copy) NSString *houseId;
+//@property (nonatomic, strong) SSWebViewContainer *webContainer;
+//@property (nonatomic, strong) UIWebView *webContainer;
 @property (nonatomic, copy) NSString *url;
-@property (nonatomic, assign) FHHouseType houseType; // 房源类型
 @property (nonatomic, assign) BOOL isShowTopTip;
 @property (nonatomic, strong) NSDictionary *traceDict;
 @property (nonatomic, copy) NSString *backUrl;
+@property (nonatomic, strong) TTHttpTask *requestTask;
+@property (nonatomic, strong) UIView *topTipView;
 @end
 
 @implementation FHHouseDetailWebViewController
-static NSString *s_oldAgent = nil;
-
 - (instancetype)initWithRouteParamObj:(TTRouteParamObj *)paramObj {
-    self = [super initWithRouteParamObj:paramObj];
-    if (self) {
-        self.realtorUserInfo = paramObj.userInfo;
-        _houseId = paramObj.allParams[@"house_id"];
-        _houseType = [paramObj.allParams[@"house_type"] integerValue];
-        _url = paramObj.allParams[@"url"];
-        _traceDict = paramObj.allParams[@"tracer"];
-        _backUrl = paramObj.allParams[@"backUrl"];
-
-        if ([_traceDict isKindOfClass:[NSDictionary class]]) {
-            NSMutableDictionary *paramsDict = [NSMutableDictionary dictionaryWithDictionary:_traceDict];
-            [paramsDict setValue:@"ios" forKey:@"os"];
-            [paramsDict setValue:@"app" forKey:@"source"];
-            [paramsDict removeObjectForKey:@"log_pb"];
-            [paramsDict removeObjectForKey:@"search_id"];
-            [paramsDict setValue:[[TTInstallIDManager sharedInstance] deviceID] forKey:@"device_id"];
-
-            NSString *getParamStr = [FHUtils getUrlFormStrFromDict:paramsDict andFirstChar:YES];
-            if ([getParamStr isKindOfClass:[NSString class]]) {
-                _url = [NSString stringWithFormat:@"%@%@",_url,getParamStr];
-                _backUrl = [NSString stringWithFormat:@"%@%@",_backUrl,getParamStr];
-            }
+    _url = paramObj.allParams[@"url"];
+    _traceDict = paramObj.allParams[@"tracer"];
+    _backUrl = paramObj.allParams[@"backUrl"];
+    
+    if ([_traceDict isKindOfClass:[NSDictionary class]]) {
+        NSMutableDictionary *paramsDict = [NSMutableDictionary dictionaryWithDictionary:_traceDict];
+        [paramsDict setValue:@"ios" forKey:@"os"];
+        [paramsDict setValue:@"app" forKey:@"source"];
+        [paramsDict removeObjectForKey:@"log_pb"];
+        [paramsDict removeObjectForKey:@"search_id"];
+        [paramsDict setValue:[[TTInstallIDManager sharedInstance] deviceID] forKey:@"device_id"];
+        
+        NSString *getParamStr = [FHUtils getUrlFormStrFromDict:paramsDict andFirstChar:YES];
+        if ([getParamStr isKindOfClass:[NSString class]]) {
+            _url = [NSString stringWithFormat:@"%@%@",_url,getParamStr];
+            _backUrl = [NSString stringWithFormat:@"%@%@",_backUrl,getParamStr];
         }
-       
+    }
+    NSMutableDictionary *params = [NSMutableDictionary dictionaryWithDictionary:paramObj.allParams];
+    [params setValue:_url forKey:@"url"];
+    TTRouteParamObj *routeObj = [[TTRouteParamObj alloc] initWithAllParams:params];
+    
+    self = [super initWithRouteParamObj:routeObj];
+    if (self) {
         if ([paramObj.userInfo.allInfo[@"showTopTip"] respondsToSelector:@selector(boolValue)]) {
             _isShowTopTip = [paramObj.userInfo.allInfo[@"showTopTip"] boolValue];
         }else
@@ -80,7 +79,6 @@ static NSString *s_oldAgent = nil;
         
         _isShowTopTip = YES;
         
-        _phoneCallViewModel = [[FHHouseDetailPhoneCallViewModel alloc]initWithHouseType:FHHouseTypeSecondHandHouse houseId:self.houseId];
     }
     return self;
 }
@@ -92,46 +90,56 @@ static NSString *s_oldAgent = nil;
         CGRect rect = CGRectMake(0.0f, 44.f + self.view.tt_safeAreaInsets.top + topTipheight, self.view.bounds.size.width, self.view.bounds.size.height - (44.f + self.view.tt_safeAreaInsets.top) - topTipheight);
         return rect;
     } else {
-        CGRect rect = CGRectMake(0.0f, 65.0f + topTipheight, self.view.bounds.size.width, self.view.bounds.size.height - 65.0f - topTipheight);
+        CGRect rect = CGRectMake(0.0f,topTipheight, self.view.bounds.size.width, self.view.bounds.size.height - 65.0f - topTipheight);
         return rect;
     }
 }
 
 - (void)setupUI
 {
-    [self setupDefaultNavBar:NO];
-    
     if (_isShowTopTip) {
-        UIView *topTipView = [[UIView alloc] initWithFrame:CGRectMake(0, [self frameForListView].origin.y - 30, self.view.frame.size.width, 30)];
-        [topTipView setBackgroundColor:[UIColor themeRed2]];
+        self.topTipView = [[UIView alloc] initWithFrame:CGRectMake(0, [self frameForListView].origin.y - 30, self.view.frame.size.width, 30)];
+        [self.topTipView setBackgroundColor:[UIColor themeRed2]];
         
-        UILabel *tipLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, topTipView.frame.size.width, topTipView.frame.size.height)];
+        UILabel *tipLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.topTipView.frame.size.width, self.topTipView.frame.size.height)];
         tipLabel.text = @"温馨提示：当前展示页面为外部链接";
         tipLabel.textColor = [UIColor themeRed3];
         tipLabel.font = [UIFont themeFontRegular:12];
         tipLabel.textAlignment = NSTextAlignmentCenter;
-        [topTipView addSubview:tipLabel];
+        [self.topTipView addSubview:tipLabel];
         
-        [self.view addSubview:topTipView];
+        [self.view addSubview:self.topTipView];
     }
     
-    self.webContainer = [[SSWebViewContainer alloc] initWithFrame:[self frameForListView]];
-    [_webContainer.ssWebView addDelegate:self];
-    [_webContainer hiddenProgressView:YES];
-    _webContainer.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    _webContainer.ssWebView.opaque = NO;
-    _webContainer.ssWebView.backgroundColor = [UIColor colorWithHexString:@"f5f5f5"];
-    _webContainer.disableEndRefresh = YES;
-    _webContainer.disableConnectCheck = YES;
-    _webContainer.ssWebView.isCheckOpenUrlNameList = YES;
-    [self.view addSubview:_webContainer];
+//    self.webContainer = [[UIWebView alloc] initWithFrame:[self frameForListView]];
+//    self.webContainer.delegate = self;
+////    [_webContainer.ssWebView addDelegate:self];
+////    [_webContainer hiddenProgressView:YES];
+////    _webContainer.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+////    _webContainer.ssWebView.opaque = NO;
+////    _webContainer.ssWebView.backgroundColor = [UIColor colorWithHexString:@"f5f5f5"];
+////    _webContainer.disableEndRefresh = YES;
+////    _webContainer.disableConnectCheck = YES;
+////    _webContainer.ssWebView.isCheckOpenUrlNameList = YES;
+//    [self.view addSubview:_webContainer];
+//
+//    @try {
+//        // 可能会出现崩溃的代码
+//        [_webContainer loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_url]]];
+//    }
+//
+//    @catch (NSException *exception) {
+//        // 捕获到的异常exception
+//        if (exception) {
+//
+//        }
+//    }
+//    @finally {
+//        // 结果处理
+//    }
+//
+//    [self tt_startUpdate];
     
-    if (_url) {
-        [_webContainer loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_url]]];
-    }
-    
-    WeakSelf;
-    NSString *loadingText = [SSCommonLogic isNewPullRefreshEnabled] ? nil : @"推荐中";
     
     if (![FHEnvContext isNetworkConnected]) {
         FHErrorView * noDataErrorView = [[FHErrorView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 0.7)];
@@ -141,10 +149,11 @@ static NSString *s_oldAgent = nil;
         __weak typeof(self) weakSelf = self;
         FHErrorView * noDataErrorViewWeak = noDataErrorView;
         noDataErrorView.retryBlock = ^{
-            if (weakSelf.webContainer.ssWebView.request && [FHEnvContext isNetworkConnected]) {
-                [noDataErrorViewWeak hideEmptyView];
-                [weakSelf.webContainer.ssWebView loadRequest:weakSelf.webContainer.ssWebView.request];
-            }
+            [weakSelf.ssWebView.ssWebContainer.ssWebView reload];
+//            if (weakSelf.webContainer.ssWebView.request && [FHEnvContext isNetworkConnected]) {
+//                [noDataErrorViewWeak hideEmptyView];
+//                [weakSelf.webContainer.ssWebView loadRequest:weakSelf.webContainer.ssWebView.request];
+//            }
         };
         
         [noDataErrorView showEmptyWithTip:@"网络异常,请检查网络链接" errorImageName:@"group-4"
@@ -163,35 +172,28 @@ static NSString *s_oldAgent = nil;
     
     [self setupUI];
     
-    _startTime = [NSDate new].timeIntervalSince1970;
     
-    [FHUserTracker writeEvent:@"go_detail" params:[self goDetailParams]];
-    [self.webContainer.ssWebView setBackgroundColor:[UIColor whiteColor]];
-    [self.view setBackgroundColor:[UIColor redColor]];
-    
+    self.ssWebView.ssWebContainer.ssWebView.isCheckOpenUrlNameList = YES;
 }
 
-#pragma mark -- UIWebViewDelegate
-
-- (void)webViewDidFinishLoad:(YSWebView *)webView
+- (void)viewWillAppear:(BOOL)animated
 {
-    [self.webContainer tt_endUpdataData];
+    [super viewWillAppear:animated];
+    
+    [self.ssWebView.ssWebContainer.ssWebView setFrame:CGRectMake(0.0f, self.topTipView.frame.size.height, self.view.frame.size.width, self.view.frame.size.height - self.topTipView.frame.origin.y - self.topTipView.frame.size.height)];
 }
 
-- (void)webViewDidStartLoad:(YSWebView *)webView
+- (void)viewDidAppear:(BOOL)animated
 {
-  
+    [super viewDidAppear:animated];
 }
 
 - (void)webView:(YSWebView *)webView didFailLoadWithError:(NSError *)error {
-    [self.webContainer tt_endUpdataData];
+    if ([self respondsToSelector:@selector(tt_endUpdataData)]) {
+        [self tt_endUpdataData];
+    }
 }
 
--(NSMutableDictionary*)goDetailParams {
-    
-    NSDictionary* params = @{};
-    return [params mutableCopy];
-}
 
 - (void)willMoveToParentViewController:(UIViewController*)parent{
     [super willMoveToParentViewController:parent];
@@ -204,25 +206,18 @@ static NSString *s_oldAgent = nil;
 - (void)didMoveToParentViewController:(UIViewController*)parent{
     [super didMoveToParentViewController:parent];
     if(!parent){
-        [FHMainApi getRequest:_backUrl query:nil params:nil completion:^(NSDictionary * _Nullable result, NSError * _Nullable error) {
-            
+      self.requestTask = [FHMainApi getRequest:_backUrl query:nil params:nil completion:^(NSDictionary * _Nullable result, NSError * _Nullable error) {
+
         }];
     }
 }
 
-- (NSString *)getUploadTraceUrlParams
+- (void)dealloc
 {
-    return @"";
+    if (self.requestTask) {
+        self.requestTask = nil;
+    }
 }
 
-static NSString * CFPropertyListRefToNSString1(CFPropertyListRef ref) {
-    if (ref == NULL) {
-        return nil;
-    }
-    if (CFGetTypeID(ref) == CFStringGetTypeID()) {
-        return (NSString *)CFBridgingRelease(ref);
-    }
-    return nil;
-}
 
 @end
