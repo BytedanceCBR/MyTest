a0cf1d539c9579e0616f0b02df6fa6cdad58bf90
zhangyuanke <zhangyuanke@bytedance.com>
2019-06-16 21:45:32 +0800

    FHUGCSearchListController 创建

---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/List/SearchList/FHUGCSearchListCell.m
@@ -0,0 +1,23 @@
+//
+//  FHUGCSearchListCell.m
+//  FHHouseUGC
+//
+//  Created by 张元科 on 2019/6/16.
+//
+
+#import "FHUGCSearchListCell.h"
+
+@implementation FHUGCSearchListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
