117db76c2652f624511775ace56e34f6a7d4cec6
zhangyuanke <zhangyuanke@bytedance.com>
2019-06-16 18:23:11 +0800

    目录结构调整

---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/List/FollowList/FHUGCFollowListController.h
@@ -0,0 +1,17 @@
+//
+//  FHUGCFollowListController.h
+//  FHHouseUGC
+//
+//  Created by 张元科 on 2019/6/16.
+//
+
+#import <UIKit/UIKit.h>
+#import "FHBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHUGCFollowListController : FHBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/List/FollowList/FHUGCFollowListController.m
@@ -0,0 +1,31 @@
+//
+//  FHUGCFollowListController.m
+//  FHHouseUGC
+//
+//  Created by 张元科 on 2019/6/16.
+//
+
+#import "FHUGCFollowListController.h"
+
+@interface FHUGCFollowListController ()
+
+@end
+
+@implementation FHUGCFollowListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
