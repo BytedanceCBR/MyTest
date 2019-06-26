11026a73f94e590a35c4a346596604269b499363
zhulijun <zhulijun.2539@bytedance.com>
2019-06-26 17:55:35 +0800

    [房源详情入口调整]

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/Base/FHDetailCommunityEntryCell.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/View/Base/FHDetailCommunityEntryCell.m
@@ -44,7 +44,7 @@
     _backView = [[UIView alloc] init];
     _backView.layer.cornerRadius = 4.0f;
     _backView.backgroundColor = [[UIColor themeRed3] colorWithAlphaComponent:0.1];
-    _backView.frame = CGRectMake(20, 10, SCREEN_WIDTH - 40, 40);
+//    _backView.frame = CGRectMake(20, 10, SCREEN_WIDTH - 40, 40);
 
     _activeCountInfoLabel = [[UILabel alloc] init];
     [_activeCountInfoLabel setContentHuggingPriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
@@ -66,6 +66,14 @@
     [_backView addSubview:_arrowView];
     _backView.clipsToBounds = YES;
     [self.contentView addSubview:_backView];
+    
+    [_backView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(20);
+        make.right.mas_equalTo(-20);
+        make.height.mas_equalTo(40);
+        make.top.mas_equalTo(10);
+        make.bottom.mas_equalTo(-10);
+    }];
 }
 
 
@@ -107,7 +115,7 @@
     CGFloat labelWidth = [self.curBubble refreshWithAvatar:model.activeUserAvatar title:model.suggestInfo color:suggestColor];
     self.curBubble.frame = CGRectMake(SCREEN_WIDTH - 40 - (6 + 12 + 4 + 4 + labelWidth + 20), 10, labelWidth + 4 + 20, 20);
 
-    labelWidth = [self.flowBubble refreshWithAvatar:model.activeUserAvatar title:nextModel.suggestInfo color:nextSuggestColor];
+    labelWidth = [self.flowBubble refreshWithAvatar:nextModel.activeUserAvatar title:nextModel.suggestInfo color:nextSuggestColor];
     self.flowBubble.frame = CGRectMake(SCREEN_WIDTH - 40 - (6 + 12 + 4 + 4 + labelWidth + 20), 40, labelWidth + 4 + 20, 20);
 }
 
@@ -167,6 +175,7 @@
         return;
     }
     self.wheelTimer = [NSTimer scheduledNoRetainTimerWithTimeInterval:4.0f target:self selector:@selector(wheelSuggestionInfo) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.wheelTimer forMode:NSRunLoopCommonModes];
     [self.wheelTimer fire];
 }
 
