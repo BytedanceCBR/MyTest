4739bc397ccb5e776a3ce6cc02bea9534d12d1c5
zhangyuanke <zhangyuanke@bytedance.com>
2019-07-08 20:47:02 +0800

    优化代码

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Post/FHPostDetailViewModel.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Post/FHPostDetailViewModel.m
@@ -199,17 +199,16 @@
         if (self.detailController.comment_count <= 0 && self.detailController.beginShowComment) {
             __weak typeof(self) weakSelf = self;
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [weakSelf reloadData];
-                [weakSelf.detailController refresh_page_view];
                 [weakSelf.detailController becomeFirstResponder_comment];
             });
-        } else {
-            __weak typeof(self) weakSelf = self;
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [weakSelf reloadData];
-                [weakSelf.detailController refresh_page_view];
-            });
         }
+        
+        // 页面布局问题修复
+        __weak typeof(self) weakSelf = self;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [weakSelf reloadData];
+            [weakSelf.detailController refresh_page_view];
+        });
     }
 }
 
