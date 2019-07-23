2b032a812f66f0533e9ba92434144450d584c754
wangzhizhou <824219521@qq.com>
2019-07-23 21:35:39 +0800

    解决首次调起

--- TTBundles/ios_tt_appruntime/TTAppRuntime/Classes/TTStartUp/Delegate/NewsBaseDelegate+Zlink.m
+++ TTBundles/ios_tt_appruntime/TTAppRuntime/Classes/TTStartUp/Delegate/NewsBaseDelegate+Zlink.m
@@ -9,6 +9,8 @@
 #import "BDUGDeepLinkManager.h"
 #import <TTRoute/TTRoute.h>
 #import "TTAdSplashMediator.h"
+#import "FHUtils.h"
+#import "FHEnvContext.h"
 
 @implementation NewsBaseDelegate(Zlink)
 -(void)deepLinkOnSchema:(NSString *)schema type:(BDUGDeepLinkType)type {
@@ -16,8 +18,17 @@
     [TTAdSplashMediator shareInstance].splashADShowType = TTAdSplashShowTypeHide;
     // 字符串若包含中文，会导致字符串转URL失败，需要进行编码转码
     NSString *schemaString = [schema stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSURL *url = [NSURL URLWithString:schemaString];
-    [[TTRoute sharedRoute] openURLByPushViewController:url];
+    if (schemaString.length > 0) {
+        BOOL hasSelectedCity = [(id)[FHUtils contentForKey:kUserHasSelectedCityKey] boolValue];
+        if (!hasSelectedCity) {
+            // no select city
+            [[NSUserDefaults standardUserDefaults] setValue:schemaString forKey:@"kFHDeepLinkFirstLaunchKey"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        } else {
+            NSURL *url = [NSURL URLWithString:schemaString];
+            [[TTRoute sharedRoute] openURLByPushViewController:url];
+        }
+    }
 
 }
 @end
