89c2005939d8faf1ac6769165dde9d12535d8d8e
wangzhizhou <824219521@qq.com>
2019-08-25 18:39:51 +0800

    评论详情页、UGC单图Cell、UGC纯标题Cell、UGC多图Cell、UGCCell底部点赞评论区 计数规则优化

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/Cells/FHUGCPureTitleCell.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Cells/FHUGCPureTitleCell.m
@@ -13,6 +13,7 @@
 #import "FHUGCCellHelper.h"
 #import "FHUGCCellOriginItemView.h"
 #import "TTRoute.h"
+#import <TTBusinessManager+StringUtils.h>
 
 #define leftMargin 20
 #define rightMargin 20
@@ -146,7 +147,7 @@
     if(commentCount == 0){
         [self.bottomView.commentBtn setTitle:@"评论" forState:UIControlStateNormal];
     }else{
-        [self.bottomView.commentBtn setTitle:cellModel.commentCount forState:UIControlStateNormal];
+        [self.bottomView.commentBtn setTitle:[TTBusinessManager formatCommentCount:commentCount] forState:UIControlStateNormal];
     }
     [self.bottomView updateLikeState:cellModel.diggCount userDigg:cellModel.userDigg];
     //内容
