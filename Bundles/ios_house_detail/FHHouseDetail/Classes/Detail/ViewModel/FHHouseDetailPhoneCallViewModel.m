5255d34af657bcc8767897164c45b5ff31244b54
xiefei.xf <xiefei.xf@bytedance.com>
2019-09-04 15:35:54 +0800

    [update]:删除无用埋点参数

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
@@ -306,30 +306,8 @@ extern NSString *const kFHPhoneNumberCacheKey;
         dict[@"page_type"] = @"realtor_detail";
         BOOL islogin = [[TTAccount sharedAccount] isLogin];
         [imdic setValue:islogin ? @"1" : @"0" forKey:@"is_login"];
-        [dict setValue:@"old_detail" forKey:@"enter_from"];
         
-        if(isPre)
-        {
-            [dict setValue:@"old_detail_button" forKey:@"element_from"];
-        }else
-        {
-            [dict setValue:@"old_detail_related" forKey:@"element_from"];
-        }
-        
-        if(isPre)
-        {
-            [dict setValue:@"old_detail_button" forKey:@"element_type"];
-        }else
-        {
-            [dict setValue:@"old_detail_related" forKey:@"element_type"];
-        }
-
-        dict[@"from"] = @"app_realtor_mainpage";
-        
-        dict[@"impr_id"] = dict[@"impr_id"] ? : @"be_null";
-        dict[@"from"] = @"app_realtor_mainpage";
-        
-        NSString *openUrlRnStr = [NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule_home&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=0&report_params=%@&im_params=%@&bundle_name=%@&is_login=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle",islogin ? @"1" : @"0"];
+        NSString *openUrlRnStr = [NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=1&report_params=%@&im_params=%@&bundle_name=%@&is_login=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle",islogin ? @"1" : @"0"];
         
         NSURL *openUrlRn = [NSURL URLWithString:openUrlRnStr];
         
