88332fe1e825db55d3117917a8d70bcd879380ad
zhulijun <zhulijun.2539@bytedance.com>
2019-09-04 15:42:33 +0800

    [UPDATE] RN详情页

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailPhoneCallViewModel.m
@@ -307,7 +307,7 @@ extern NSString *const kFHPhoneNumberCacheKey;
         BOOL islogin = [[TTAccount sharedAccount] isLogin];
         [imdic setValue:islogin ? @"1" : @"0" forKey:@"is_login"];
         
-        NSString *openUrlRnStr = [NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=1&report_params=%@&im_params=%@&bundle_name=%@&is_login=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle",islogin ? @"1" : @"0"];
+        NSString *openUrlRnStr = [NSString stringWithFormat:@"sslocal://react?module_name=FHRNAgentDetailModule_home&realtorId=%@&can_multi_preload=%ld&channelName=f_realtor_detail&debug=0&report_params=%@&im_params=%@&bundle_name=%@&is_login=%@",contactPhone.realtorId,isPre ? 1 : 0,[FHUtils getJsonStrFrom:dict],[FHUtils getJsonStrFrom:imdic],@"agent_detail.bundle",islogin ? @"1" : @"0"];
         
         NSURL *openUrlRn = [NSURL URLWithString:openUrlRnStr];
         
