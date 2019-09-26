5b2d04d44c6b54273def6f212c93a3643d706c1e
xiefei.xf <xiefei.xf@bytedance.com>
2019-09-26 21:15:15 +0800

    [update]:添加VR lottie json文件

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/MultiMediaShow/View/FHMultiMediaVRImageCell.m
@@ -0,0 +1,12 @@
+//
+//  FHMultiMediaVRImageCell.m
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2019/9/26.
+//
+
+#import "FHMultiMediaVRImageCell.h"
+
+@implementation FHMultiMediaVRImageCell
+
+@end
