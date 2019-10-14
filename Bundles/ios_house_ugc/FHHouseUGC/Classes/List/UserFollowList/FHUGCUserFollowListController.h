6559917f18604ff1c50d186b878b28ee7e5b33ba
zhangyuanke <zhangyuanke@bytedance.com>
2019-10-14 14:45:14 +0800

    ugc 创建列表类

---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/List/UserFollowList/FHUGCUserFollowListController.h
@@ -0,0 +1,22 @@
+//
+//  FHUGCUserFollowListController.h
+//  FHHouseUGC
+//
+//  Created by 张元科 on 2019/10/14.
+//
+
+#import <UIKit/UIKit.h>
+#import "FHBaseViewController.h"
+#import "FHUGCSearchBar.h"
+#import "FHCommunityList.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+// UGC 搜索
+@interface FHUGCUserFollowListController : FHBaseViewController
+
+@property (nonatomic, strong)     FHUGCSearchBar     *naviBar;
+
+@end
+
+NS_ASSUME_NONNULL_END
