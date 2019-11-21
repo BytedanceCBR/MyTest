6e2ff23759297f1e036c332d37111205b9973185
xiefei.xf <xiefei.xf@bytedance.com>
2019-11-21 23:11:15 +0800

    [bugfix]:修改placeholder

--- Bundles/ios_house_list/FHHouseList/Classes/House/ViewModel/FHHouseListViewModel.m
+++ Bundles/ios_house_list/FHHouseList/Classes/House/ViewModel/FHHouseListViewModel.m
@@ -1501,6 +1501,10 @@ extern NSString *const INSTANT_DATA_KEY;
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (_showPlaceHolder) {
+        if (self.houseType == FHHouseTypeNewHouse) {
+            FHPlaceHolderCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([FHPlaceHolderCell class])];
+            return cell;
+        }
 
         if(self.commute){
             FHPlaceHolderCell *cell = [tableView dequeueReusableCellWithIdentifier:kFHHouseListPlaceholderCellId];
@@ -1622,6 +1626,9 @@ extern NSString *const INSTANT_DATA_KEY;
         height = 105;
     }
     if (_showPlaceHolder) {
+        if (self.houseType == FHHouseTypeNewHouse) {
+            return 118;
+        }
         return height;
     }
     NSString *identifier = @"";
