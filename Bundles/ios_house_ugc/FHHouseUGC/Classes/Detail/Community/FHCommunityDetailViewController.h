f707fc20bf9fc88d8805bcedd3718d2ffdcb0fc9
xiesiming <xiesiming@bytedance.com>
2019-11-27 20:40:57 +0800

    修复偶现的邻里cell高度不对的问题

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Community/FHCommunityDetailViewController.h
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Community/FHCommunityDetailViewController.h
@@ -20,19 +20,19 @@ NS_ASSUME_NONNULL_BEGIN
 // 圈子详情页
 @interface FHCommunityDetailViewController : FHBaseViewController
 
-@property(nonatomic, copy) NSString *communityId;
-@property(nonatomic, strong) FHCommunityDetailHeaderView *headerView;
-@property(nonatomic, strong) FHCommunityDetailSegmentView *segmentView;
-@property(nonatomic, strong) FHUGCFollowButton *rightBtn;
-@property(nonatomic, strong) UILabel *titleLabel;
-@property(nonatomic, strong) UILabel *subTitleLabel;
-@property(nonatomic, strong) UIView *titleContainer;
+@property (nonatomic, copy) NSString *communityId;
+@property (nonatomic, strong) FHCommunityDetailHeaderView *headerView;
+@property (nonatomic, strong) FHCommunityDetailSegmentView *segmentView;
+@property (nonatomic, strong) FHUGCFollowButton *rightBtn;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *subTitleLabel;
+@property (nonatomic, strong) UIView *titleContainer;
 //发布按钮
 @property (nonatomic, strong) UIButton *publishBtn;
 //群聊按钮
-@property(nonatomic, strong) UIButton *groupChatBtn;
+@property (nonatomic, strong) UIButton *groupChatBtn;
 //群聊红泡提示按钮
-@property(nonatomic, strong) TTBadgeNumberView *bageView;
+@property (nonatomic, strong) TTBadgeNumberView *bageView;
 
 @end
 
