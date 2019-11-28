ec4e1ddc0fc3ef6ef076a4d181c56c6e2be53ea5
xiefei.xf <xiefei.xf@bytedance.com>
2019-11-28 17:47:14 +0800

    update:修改topbar

--- Bundles/ios_house_home/FHHouseHome/Classes/views/FHHomeMainTopView.m
+++ Bundles/ios_house_home/FHHouseHome/Classes/views/FHHomeMainTopView.m
@@ -22,7 +22,7 @@
 @interface FHHomeMainTopView()
 
 @property(nonatomic,strong)HMSegmentedControl *segmentControl;
-@property (nonatomic, strong) UIView *topUnAvalibleCityContainer;
+@property (nonatomic, strong) UIView *topBackCityContainer;
 @property (nonatomic, strong) SSThemedImageView *backgroundImageView;
 @property(nonatomic, strong) UIButton * changeCountryBtn;
 @property(nonatomic, strong) UILabel * countryLabel;
@@ -64,66 +64,11 @@
     self.backgroundImageView.layer.zPosition = -1;
     self.backgroundImageView.userInteractionEnabled = YES;
     
-    [self setupCityButton];
-    
     WeakSelf;
     [[FHEnvContext sharedInstance].configDataReplay subscribeNext:^(id  _Nullable x) {
         StrongSelf;
         [self showUnValibleCity];
     }];
-}
-
-- (void)setupCityButton
-{
-    UIButton *citySwichButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.changeCountryBtn = citySwichButton;
-    [self addSubview:citySwichButton];
-    citySwichButton.layer.cornerRadius = 20;
-    citySwichButton.layer.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.1f].CGColor;
-    citySwichButton.layer.shadowOffset = CGSizeMake(0.f, 2.f);
-    citySwichButton.layer.shadowRadius = 6.f;
-    citySwichButton.layer.shadowOpacity = 1.f;
-    [citySwichButton.titleLabel setFont:[UIFont themeFontRegular:14]];
-    citySwichButton.backgroundColor = [UIColor whiteColor];
-    NSString *text = [FHEnvContext getCurrentUserDeaultCityNameFromLocal];
-    [citySwichButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self).offset(20);
-        make.height.mas_equalTo(40);
-        make.bottom.equalTo(self.mas_bottom).offset(-12);
-    }];
-    
-    [[[citySwichButton rac_signalForControlEvents:UIControlEventTouchUpInside] takeUntil:self.rac_willDeallocSignal] subscribeNext:^(__kindof UIControl * _Nullable x){
-         [[FHHomeConfigManager sharedInstance].fhHomeBridgeInstance jumpCountryList:self.viewController];
-    }];
-    
-    UIImageView *imageButtonLeftIcon = [UIImageView new];
-    [citySwichButton addSubview:imageButtonLeftIcon];
-    self.cityImageButtonLeftIcon = imageButtonLeftIcon;
-    //    [imageButtonLeftIcon setImage:[UIImage imageNamed:@"combined-shape-1"]];
-    [imageButtonLeftIcon mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(citySwichButton).offset(14);
-        make.height.mas_equalTo(18);
-        make.centerY.equalTo(citySwichButton);
-        make.width.mas_equalTo(18);
-    }];
-    
-    UILabel *label = [[UILabel alloc] init];
-    label.font = [UIFont themeFontRegular:14];
-    label.textColor = [UIColor themeGray1];
-    label.numberOfLines = 1;
-    label.text = [FHEnvContext getCurrentUserDeaultCityNameFromLocal];
-    self.countryLabel = label;
-    [self addSubview:self.countryLabel];
-    
-    [self.countryLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(imageButtonLeftIcon.mas_right).offset(2);
-        make.centerY.equalTo(self);
-        make.height.mas_equalTo(20);
-        make.width.mas_equalTo(label.text.length * 14);
-        make.right.mas_equalTo(citySwichButton.mas_right).offset(-14);
-    }];
-    
-    [self.countryLabel sizeToFit];
 }
 
 - (void)setupSetmentedControl {
@@ -145,102 +90,121 @@
     _segmentControl.selectionIndicatorHeight = 12.0f;
     _segmentControl.selectionIndicatorImage = [UIImage imageNamed:@"fh_ugc_segment_selected"];
     
-    [self addSubview:_segmentControl];
-    
     __weak typeof(self) weakSelf = self;
     _segmentControl.indexChangeBlock = ^(NSInteger index) {
     };
     
     _segmentControl.indexRepeatBlock = ^(NSInteger index) {
     };
+    
+    
+    [self.topBackCityContainer addSubview:_segmentControl];
+    
+    [_segmentControl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.changeCountryBtn.mas_right).offset(0);
+        make.height.mas_equalTo(52);
+        make.bottom.equalTo(self.topBackCityContainer.mas_bottom).offset(0);
+        make.width.mas_equalTo(108);
+    }];
 }
 
 - (void)showUnValibleCity
 {
+    [self updateContainerCity:[FHEnvContext isCurrentCityNormalOpen]];
+}
+
+- (void)updateContainerCity:(BOOL)isOpen
+{
+    if (self.topBackCityContainer) {
+        [self.topBackCityContainer removeFromSuperview];
+        self.topBackCityContainer = nil;
+    }
     
     FHConfigDataModel *dataModel = [[FHEnvContext sharedInstance] getConfigFromCache];
-    if (dataModel.cityAvailability && [dataModel.cityAvailability.enable respondsToSelector:@selector(boolValue)] &&[dataModel.cityAvailability.enable boolValue] == false) {
-        
-        if (self.topUnAvalibleCityContainer) {
-            [self.topUnAvalibleCityContainer removeFromSuperview];
-            self.topUnAvalibleCityContainer = nil;
-        }
-        
-        self.topUnAvalibleCityContainer = [[UIView alloc] init];
-        [self.backgroundImageView addSubview:self.topUnAvalibleCityContainer];
-        [self.backgroundImageView bringSubviewToFront:self.topUnAvalibleCityContainer];
-        
-        [self.topUnAvalibleCityContainer mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.edges.equalTo(self.backgroundImageView);
-        }];
-        if (dataModel.cityAvailability.backgroundColor) {
-            [self.topUnAvalibleCityContainer setBackgroundColor:[UIColor colorWithHexString:dataModel.cityAvailability.backgroundColor]];
-        }
-        
-        CGFloat padingTop = 8;
-        if ([TTDeviceHelper isIPhoneXSeries]) {
-            padingTop = 20;
-        }
-        BOOL isLarge320 = [TTDeviceHelper isScreenWidthLarge320];
-        CGFloat tipsFontSize = 14.0;
-        CGFloat leftOffset = 14;
-        // 适配小屏幕
-        if (!isLarge320) {
-            leftOffset = 10;
-            tipsFontSize = 12;
-        }
-        CGFloat widthOffset = leftOffset * 2;
-        UILabel *cityLabel = [[UILabel alloc] init];
-        cityLabel.textColor = [UIColor tt_themedColorForKey:@"grey1"];
-        cityLabel.text = dataModel.currentCityName;
-        cityLabel.font = [UIFont themeFontRegular:14];
-        
-        UIButton *citySwichButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [self.topUnAvalibleCityContainer addSubview:citySwichButton];
-        citySwichButton.layer.cornerRadius = 20;
-        citySwichButton.layer.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.1f].CGColor;
-        citySwichButton.layer.shadowOffset = CGSizeMake(0.f, 2.f);
-        citySwichButton.layer.shadowRadius = 6.f;
-        citySwichButton.layer.shadowOpacity = 1.f;
-        [citySwichButton.titleLabel setFont:[UIFont themeFontRegular:14]];
-        citySwichButton.backgroundColor = [UIColor whiteColor];
-        [citySwichButton mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.equalTo(self.topUnAvalibleCityContainer).offset(20);
-            make.height.mas_equalTo(40);
-            make.bottom.equalTo(self.topUnAvalibleCityContainer.mas_bottom).offset(-12);
-            make.width.mas_equalTo(dataModel.currentCityName.length * 14 + 24 + widthOffset); // button width
-        }];
-        [citySwichButton setTitleEdgeInsets:UIEdgeInsetsMake(0, 22, 0, 0)];
-        [citySwichButton addTarget:self withActionBlock:^{
-            NSURL *url = [[NSURL alloc] initWithString:@"sslocal://city_list"];
-            [[TTRoute sharedRoute] openURLByPushViewController:url userInfo:NULL];
-        } forControlEvent:UIControlEventTouchUpInside];
-        
-        UIImageView *imageButtonLeftIcon = [UIImageView new];
-        [citySwichButton addSubview:imageButtonLeftIcon];
-        [imageButtonLeftIcon setImage:[UIImage imageNamed:@"combined-shape-1"]];
-        [imageButtonLeftIcon mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.equalTo(citySwichButton).offset(leftOffset);
-            make.height.mas_equalTo(18);
-            make.centerY.equalTo(citySwichButton);
-            make.width.mas_equalTo(18);
-        }];
-        
-        [citySwichButton addSubview:cityLabel];
-        [cityLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(imageButtonLeftIcon.mas_right).offset(2);
-            make.height.mas_equalTo(21);
-            make.centerY.mas_equalTo(imageButtonLeftIcon);
-        }];
-        
+
+    if (self.topBackCityContainer) {
+        [self.topBackCityContainer removeFromSuperview];
+        self.topBackCityContainer = nil;
+    }
+    
+    self.topBackCityContainer = [[UIView alloc] init];
+    [self.backgroundImageView addSubview:self.topBackCityContainer];
+    [self.backgroundImageView bringSubviewToFront:self.topBackCityContainer];
+    
+    [self.topBackCityContainer mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.backgroundImageView);
+    }];
+    if (dataModel.cityAvailability.backgroundColor) {
+        [self.topBackCityContainer setBackgroundColor:[UIColor colorWithHexString:dataModel.cityAvailability.backgroundColor]];
+    }
+    
+    CGFloat padingTop = 8;
+    if ([TTDeviceHelper isIPhoneXSeries]) {
+        padingTop = 20;
+    }
+    BOOL isLarge320 = [TTDeviceHelper isScreenWidthLarge320];
+    CGFloat tipsFontSize = 14.0;
+    CGFloat leftOffset = 14;
+    // 适配小屏幕
+    if (!isLarge320) {
+        leftOffset = 10;
+        tipsFontSize = 12;
+    }
+    CGFloat widthOffset = leftOffset * 2;
+    UILabel *cityLabel = [[UILabel alloc] init];
+    cityLabel.textColor = [UIColor tt_themedColorForKey:@"grey1"];
+    cityLabel.text = dataModel.currentCityName;
+    cityLabel.font = [UIFont themeFontRegular:14];
+    
+    UIButton *citySwichButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.changeCountryBtn = citySwichButton;
+    [self.topBackCityContainer addSubview:citySwichButton];
+    citySwichButton.layer.cornerRadius = 20;
+    citySwichButton.layer.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.1f].CGColor;
+    citySwichButton.layer.shadowOffset = CGSizeMake(0.f, 2.f);
+    citySwichButton.layer.shadowRadius = 6.f;
+    citySwichButton.layer.shadowOpacity = 1.f;
+    [citySwichButton.titleLabel setFont:[UIFont themeFontRegular:14]];
+    citySwichButton.backgroundColor = [UIColor whiteColor];
+    [citySwichButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.topBackCityContainer).offset(20);
+        make.height.mas_equalTo(40);
+        make.bottom.equalTo(self.topBackCityContainer.mas_bottom).offset(-12);
+        make.width.mas_equalTo(dataModel.currentCityName.length * 14 + 24 + widthOffset); // button width
+    }];
+    
+    [citySwichButton setTitleEdgeInsets:UIEdgeInsetsMake(0, 22, 0, 0)];
+    [citySwichButton addTarget:self withActionBlock:^{
+        NSURL *url = [[NSURL alloc] initWithString:@"sslocal://city_list"];
+        [[TTRoute sharedRoute] openURLByPushViewController:url userInfo:NULL];
+    } forControlEvent:UIControlEventTouchUpInside];
+    
+    UIImageView *imageButtonLeftIcon = [UIImageView new];
+    [citySwichButton addSubview:imageButtonLeftIcon];
+    [imageButtonLeftIcon setImage:[UIImage imageNamed:@"combined-shape-1"]];
+    [imageButtonLeftIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(citySwichButton).offset(leftOffset);
+        make.height.mas_equalTo(18);
+        make.centerY.equalTo(citySwichButton);
+        make.width.mas_equalTo(18);
+    }];
+    
+    [citySwichButton addSubview:cityLabel];
+    [cityLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(imageButtonLeftIcon.mas_right).offset(2);
+        make.height.mas_equalTo(21);
+        make.centerY.mas_equalTo(imageButtonLeftIcon);
+    }];
+    
+    if (!isOpen) {
         UIImageView *imageRightView = [UIImageView new];
-        [self.topUnAvalibleCityContainer addSubview:imageRightView];
+        [self.topBackCityContainer addSubview:imageRightView];
         imageRightView.layer.opacity = 0.3;
         
         [imageRightView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.right.equalTo(self.topUnAvalibleCityContainer).offset(0);
+            make.right.equalTo(self.topBackCityContainer).offset(0);
             make.height.mas_equalTo(52);
-            make.bottom.equalTo(self.topUnAvalibleCityContainer.mas_bottom).offset(0);
+            make.bottom.equalTo(self.topBackCityContainer.mas_bottom).offset(0);
             make.width.mas_equalTo(108);
         }];
         
@@ -249,7 +213,7 @@
         topTipForCityLabel.text = @"找房服务即将开通，敬请期待";
         topTipForCityLabel.font = [UIFont themeFontRegular:tipsFontSize];
         topTipForCityLabel.textColor = [UIColor tt_themedColorForKey:@"grey3"];
-        [self.topUnAvalibleCityContainer addSubview:topTipForCityLabel];
+        [self.topBackCityContainer addSubview:topTipForCityLabel];
         
         [topTipForCityLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.equalTo(citySwichButton.mas_right).offset(10);
@@ -262,14 +226,11 @@
         if (dataModel.cityAvailability.iconImage.url) {
             [imageRightView bd_setImageWithURL:[NSURL URLWithString:dataModel.cityAvailability.iconImage.url]];
         }
-        
     }else
     {
-        if (self.topUnAvalibleCityContainer) {
-            [self.topUnAvalibleCityContainer removeFromSuperview];
-            self.topUnAvalibleCityContainer = nil;
-        }
+        [self setupSetmentedControl];
     }
+   
 }
 
 - (NSArray *)getSegmentTitles {
