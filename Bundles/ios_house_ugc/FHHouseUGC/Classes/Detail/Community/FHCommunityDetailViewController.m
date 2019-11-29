2e1f22c609557b36b49ae27c68981734c2633185
wangzhizhou <824219521@qq.com>
2019-11-29 18:18:24 +0800

    添加提问发布器相关埋点

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Community/FHCommunityDetailViewController.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Detail/Community/FHCommunityDetailViewController.m
@@ -368,12 +368,11 @@
 }
 
 - (void)gotoWendaPublish {
-    // JOKER: ASK PUBLISH
-//
-//    NSMutableDictionary *params = @{}.mutableCopy;
-//    params[UT_ELEMENT_TYPE] = @"feed_icon";
-//    params[UT_PAGE_TYPE] = self.tracerDict[UT_PAGE_TYPE]?:UT_BE_NULL;
-//    TRACK_EVENT(@"click_options", params);
+
+    NSMutableDictionary *params = @{}.mutableCopy;
+    params[UT_ELEMENT_TYPE] = @"question_icon";
+    params[UT_PAGE_TYPE] = self.tracerDict[UT_PAGE_TYPE]?:UT_BE_NULL;
+    TRACK_EVENT(@"click_options", params);
     
     [self.viewModel gotoWendaPublish];
 }
