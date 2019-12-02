d5fd01a8a9b3b3611128a57e405da40519b22474
zhangyuanke <zhangyuanke@bytedance.com>
2019-12-02 17:05:20 +0800

    加群按钮逻辑展示

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailContactViewModel.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/ViewModel/FHHouseDetailContactViewModel.m
@@ -393,7 +393,7 @@ NSString *const kFHDetailLoadingNotification = @"kFHDetailLoadingNotification";
     _socialInfo = socialInfo;
     NSString *groupChatTitle = @"";// 隐藏
     if (socialInfo) {
-        if (socialInfo.socialGroupInfo.socialGroupId.length > 0 && (socialInfo.socialGroupInfo.userAuth > UserAuthTypeNormal || [socialInfo.socialGroupInfo.chatStatus.conversationId integerValue] > 0)) {
+        if (socialInfo.socialGroupInfo.socialGroupId.length > 0 && ([socialInfo.socialGroupInfo.chatStatus.conversationId integerValue] > 0)) {
             groupChatTitle = socialInfo.groupChatLinkTitle.length > 0 ? socialInfo.groupChatLinkTitle : @"加入看盘群";
         } else {
             groupChatTitle = @"";
