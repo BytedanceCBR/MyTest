b6e39832d8523ef8430c755afe41a6fcd5bab8e0
xiefei.xf <xiefei.xf@bytedance.com>
2019-12-04 21:46:00 +0800

    update:修改房源item高度

--- Bundles/ios_house_home/FHHouseHome/Classes/manager/FHHomeCellHelper.h
+++ Bundles/ios_house_home/FHHouseHome/Classes/manager/FHHomeCellHelper.h
@@ -24,7 +24,7 @@ typedef NS_ENUM(NSUInteger, FHHomeCellViewType) {
 
 #define kFHHomeBannerDefaultHeight 60.0 //banner高度
 
-#define kFHHomeHouseItemHeight 96.0 //banner高度
+#define kFHHomeHouseItemHeight 88.0 //banner高度
 
 //#define kFHHomeIconRowCount 5 //每行icon个数
 
