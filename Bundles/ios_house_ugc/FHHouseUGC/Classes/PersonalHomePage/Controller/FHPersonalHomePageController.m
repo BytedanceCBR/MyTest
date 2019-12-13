18b141035e8ed43bd9f871b1642af18cf8ef56ae
xiesiming <xiesiming@bytedance.com>
2019-12-13 16:06:50 +0800

    修复feed中上拉刷新抖动的问题

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/PersonalHomePage/Controller/FHPersonalHomePageController.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/PersonalHomePage/Controller/FHPersonalHomePageController.m
@@ -256,12 +256,15 @@
     UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0.001)];
     _tableView.tableFooterView = footerView;
     
-    _tableView.sectionFooterHeight = 0.0;
-    
-    _tableView.estimatedRowHeight = 0;
+//    _tableView.sectionFooterHeight = 0.0;
+//    
+//    _tableView.estimatedRowHeight = 0;
     
     if (@available(iOS 11.0 , *)) {
         _tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        _tableView.estimatedRowHeight = 0;
+        _tableView.estimatedSectionFooterHeight = 0;
+        _tableView.estimatedSectionHeaderHeight = 0;
     }
     
     if ([TTDeviceHelper isIPhoneXSeries]) {
