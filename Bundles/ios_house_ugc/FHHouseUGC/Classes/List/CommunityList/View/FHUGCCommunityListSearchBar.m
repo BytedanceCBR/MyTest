ea4401567db08307e02067ec0d2538defd096e02
wangzhizhou <824219521@qq.com>
2019-12-25 11:38:55 +0800

    关注按钮UI改动，圈子选择列表页搜索框样式改动

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/List/CommunityList/View/FHUGCCommunityListSearchBar.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/List/CommunityList/View/FHUGCCommunityListSearchBar.m
@@ -35,8 +35,6 @@
     // searchAreaPanel
     _searchAreaPanel = [UIButton buttonWithType:UIButtonTypeCustom];
     _searchAreaPanel.backgroundColor = [UIColor themeGray7];
-    _searchAreaPanel.layer.masksToBounds = YES;
-    _searchAreaPanel.layer.cornerRadius = 4.0;
     [_searchAreaPanel addTarget:self action:@selector(onSearchClicked) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:_searchAreaPanel];
 
@@ -56,10 +54,17 @@
     [self addSubview:_separatorLine];
 }
 
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    self.searchAreaPanel.layer.masksToBounds = YES;
+    self.searchAreaPanel.layer.cornerRadius = 17.0f;
+}
 -(void)initConstraints{
 
     [self.searchAreaPanel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(self).insets(UIEdgeInsetsMake(4, 20, 10, 20));
+        make.edges.mas_equalTo(self).insets(UIEdgeInsetsMake(3, 20, 9, 20));
     }];
 
     [self.searchIcon mas_makeConstraints:^(MASConstraintMaker *make) {
