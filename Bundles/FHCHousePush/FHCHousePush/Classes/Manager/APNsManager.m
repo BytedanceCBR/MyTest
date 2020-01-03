b126021c676964784baddd3fe8ed488b8fa76fcf
xiesiming <xiesiming@bytedance.com>
2020-01-03 17:48:15 +0800

    解决push进来先关闭引导页

--- Bundles/FHCHousePush/FHCHousePush/Classes/Manager/APNsManager.m
+++ Bundles/FHCHousePush/FHCHousePush/Classes/Manager/APNsManager.m
@@ -36,6 +36,7 @@
 #import "FHBaseViewController.h"
 #import "TTUIResponderHelper.h"
 #import "UIViewController+TTMovieUtil.h"
+#import "FHIntroduceManager.h"
 
 extern NSString * const TTArticleTabBarControllerChangeSelectedIndexNotification;
 
@@ -102,7 +103,10 @@ static APNsManager *_sharedManager = nil;
 
 - (void)handleRemoteNotification:(NSDictionary *)userInfo
 {
-    
+    //当push进来引导页已经在显示了，则关闭
+    if([FHIntroduceManager sharedInstance].isShowing){
+        [[FHIntroduceManager sharedInstance] hideIntroduceView];
+    }
     //news_notification_view埋点，用户在后台点击推送时上报，如果有rid则上报rid
     NSString *rid = [userInfo tt_stringValueForKey:@"rid"];
     NSString *postBack = [userInfo tt_stringValueForKey:@"post_back"];
