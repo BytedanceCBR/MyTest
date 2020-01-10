e8a0aa0a77abcabacfef3045cd25d084db03ad73
guchunhui <guchunhui@bytedance.com>
2020-01-10 15:08:26 +0800

    feature: 法务需求-幸福里App合规操作

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/PermissionAlert/FHPermissionAlertViewController.h
@@ -0,0 +1,18 @@
+//
+//  FHPermissionAlertViewController.h
+//  FHHouseBase
+//
+//  Created by 春晖 on 2019/12/31.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHPermissionAlertViewController : UIViewController
+
++(void)show;
+
+@end
+
+NS_ASSUME_NONNULL_END
