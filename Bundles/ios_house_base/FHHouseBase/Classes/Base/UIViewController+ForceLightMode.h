6d6514e9b0a59dc46a4c2deddc44d284ec401d79
zhangyuanke <zhangyuanke@bytedance.com>
2020-02-10 15:09:55 +0800

    适配暗黑模式状态栏

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Base/UIViewController+ForceLightMode.h
@@ -0,0 +1,20 @@
+//
+//  UIViewController+ForceLightMode.h
+//  FHBHouseBase
+//
+//  Created by 春晖 on 2020/1/17.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (ForceLightMode)
+
+@end
+
+@interface UIApplication (ForceLightMode)
+
+@end
+
+NS_ASSUME_NONNULL_END
