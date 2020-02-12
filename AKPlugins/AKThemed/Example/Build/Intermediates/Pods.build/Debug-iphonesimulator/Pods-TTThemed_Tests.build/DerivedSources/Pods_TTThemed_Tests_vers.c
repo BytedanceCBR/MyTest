a213053e808d30aa67894c7a86b7e81caa4a4977
zhangjing <zhangjing.2018@bytedance.com>
2020-02-12 17:46:40 +0800

    [UPDATE] 删除废弃的本地pod库

--- AKPlugins/AKThemed/Example/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-TTThemed_Tests.build/DerivedSources/Pods_TTThemed_Tests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_TTThemed_TestsVersionString[];
- extern const double Pods_TTThemed_TestsVersionNumber;
-
- const unsigned char Pods_TTThemed_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_TTThemed_Tests  PROJECT:Pods-1" "\n";
- const double Pods_TTThemed_TestsVersionNumber __attribute__ ((used)) = (double)1.;
--- AKPlugins/AKThemed/Example/Build/Intermediates/Pods.build/Debug-iphonesimulator/TTThemed.build/Objects-normal/x86_64/TTThemed-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/fengjingjun/work/Demo/TTPods/tt_pods_theme/Example/Pods/Target\ Support\ Files/TTThemed/TTThemed-dummy.m \
-  /Users/fengjingjun/work/Demo/TTPods/tt_pods_theme/Example/Pods/Target\ Support\ Files/TTThemed/TTThemed-prefix.pch
