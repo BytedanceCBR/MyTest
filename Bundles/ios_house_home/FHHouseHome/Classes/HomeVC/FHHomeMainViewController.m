ed262c207c5f0b23781c7fa526c4e1d902001923
王志舟 <wangzhizhou@bytedance.com>
2020-02-19 12:19:51 +0800

    bugfix

--- Bundles/ios_house_home/FHHouseHome/Classes/HomeVC/FHHomeMainViewController.m
+++ Bundles/ios_house_home/FHHouseHome/Classes/HomeVC/FHHomeMainViewController.m
@@ -354,10 +354,8 @@ static NSString * const kFUGCPrefixStr = @"fugc";
     
     [TTAppUpdateHelper sharedInstance].delegate = self;
     [[TTAppUpdateHelper sharedInstance] checkVersionUpdateWithInstallID:iidValue deviceID:didValue channel:channelValue aid:aidValue checkVersionBaseUrl:baseUrl correctVC:self completionBlock:^(__kindof UIView *view, NSError * _Nullable error) {
-        if(![FHPopupViewManager shared].presenter.isPresenting){
-            [self.view addSubview:view];
-            [[FHPopupViewManager shared] outerPopupViewShow];
-        }
+        [self.view addSubview:view];
+        [[FHPopupViewManager shared] outerPopupViewShow];
     } updateBlock:^(BOOL isTestFlightUpdate, NSString *downloadUrl) {
         //        if (!downloadUrl) {
         //            return;
