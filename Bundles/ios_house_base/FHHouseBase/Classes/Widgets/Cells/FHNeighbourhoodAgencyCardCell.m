9233b9df85e44191014ca82d4835e73c98cd140e
liuyu.shelley <liuyu.shelley@bytedance.com>
2020-02-26 18:35:00 +0800

    [UPDATE]：列表页经济人服务分修改

--- Bundles/ios_house_base/FHHouseBase/Classes/Widgets/Cells/FHNeighbourhoodAgencyCardCell.m
+++ Bundles/ios_house_base/FHHouseBase/Classes/Widgets/Cells/FHNeighbourhoodAgencyCardCell.m
@@ -37,6 +37,8 @@
 @property(nonatomic, strong) UIButton *imBtn;
 @property(nonatomic, strong) UILabel *name;
 @property(nonatomic, strong) UILabel *agency;
+@property (nonatomic, strong) UILabel     *score;
+@property (nonatomic, strong) UILabel     *scoreDescription;
 
 @property(nonatomic, strong) FHHouseNeighborAgencyModel *modelData;
 @property(nonatomic, strong) FHHouseDetailPhoneCallViewModel *phoneCallViewModel;
@@ -138,6 +140,17 @@
     _agency.textColor = [UIColor themeGray3];
     _agency.textAlignment = NSTextAlignmentLeft;
     [self.bottomInfoView addSubview:_agency];
+    
+    _score = [UILabel createLabel:@"" textColor:@"" fontSize:14];
+    _score.textColor = [UIColor themeGray1];
+     _score.font = [UIFont themeFontDINAlternateBold:14];
+    _score.textAlignment = NSTextAlignmentLeft;
+    [self.bottomInfoView addSubview:_score];
+    
+    _scoreDescription= [UILabel createLabel:@"" textColor:@"" fontSize:14];
+    _scoreDescription.textColor = [UIColor colorWithHexStr:@"6d7278"];
+    _scoreDescription.textAlignment = NSTextAlignmentLeft;
+    [self.bottomInfoView addSubview:_scoreDescription];
 
     [self.topInfoView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(neighbourhoodInfoClick:)]];
     [self.bottomInfoView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(realtorInfoClick:)]];
@@ -209,10 +222,19 @@
         make.height.mas_equalTo(22);
     }];
     [self.agency mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.name.mas_bottom);
+        make.centerY.mas_equalTo(self.name);
         make.height.mas_equalTo(17);
-        make.left.mas_equalTo(self.avator.mas_right).offset(10);
-        make.right.mas_lessThanOrEqualTo(self.imBtn.mas_left).offset(-15);
+        make.left.mas_equalTo(self.name.mas_right).offset(4);
+        make.right.mas_lessThanOrEqualTo(self.imBtn.mas_left).offset(-4);
+    }];
+    [self.score mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.name.mas_bottom).offset(8);
+        make.left.equalTo(self.name);
+    }];
+    [self.scoreDescription mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.score.mas_right).offset(3);
+        make.centerY.mas_equalTo(self.score);
+        make.right.mas_lessThanOrEqualTo(self.imBtn.mas_left).offset(-4);
     }];
     [self.licenceIcon mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(self.name.mas_right).offset(5);
@@ -251,13 +273,16 @@
         if (model.contactModel) {
             self.name.text = model.contactModel.realtorName;
             self.agency.text = model.contactModel.agencyName;
-            if (IS_EMPTY_STRING(self.agency.text)) {
+            self.score.text = model.contactModel.realtorScoreDisplay;
+            self.scoreDescription.text = model.contactModel.realtorScoreDescription;
+            if (IS_EMPTY_STRING(self.scoreDescription.text) || IS_EMPTY_STRING(self.score.text)) {
                 [self.name mas_remakeConstraints:^(MASConstraintMaker *make) {
                     make.centerY.mas_equalTo(self.avator);
                     make.left.mas_equalTo(self.avator.mas_right).offset(10);
                     make.height.mas_equalTo(22);
                 }];
-
+                self.score.hidden = YES;
+                self.scoreDescription.hidden = YES;
                 [self.licenceIcon updateConstraintsIfNeeded];
             }
             if (model.contactModel.avatarUrl.length > 0) {
