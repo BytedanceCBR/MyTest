fd80009d19cc164249fc0895e075301e54b2ec36
luowentao <luowentao@bytedance.com>
2020-04-01 21:43:27 +0800

    更新写回答

--- AKPlugins/AKWDPlugin/Pages/WDList/View/WDListBottomButton.m
+++ AKPlugins/AKWDPlugin/Pages/WDList/View/WDListBottomButton.m
@@ -77,7 +77,7 @@
 - (void)setupUI {
     self.backgroundColor = [UIColor whiteColor];
     self.iconImageView = [[UIImageView alloc] init];
-    self.iconImageView.image = ICON_FONT_IMG(24, @"\U0000e6b3", [UIColor themeRed1]);
+    self.iconImageView.image = ICON_FONT_IMG(24, @"\U0000e6b3", [UIColor themeOrange1]);
     [self addSubview:_iconImageView];
     self.titleLabel = [self labelWithFont:[UIFont themeFontRegular:16] textColor:[UIColor themeOrange1]];
     [self addSubview:_titleLabel];
