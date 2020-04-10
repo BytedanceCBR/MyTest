8da14391fa087923c548c221361def33cc402ca5
王志舟 <wangzhizhou@bytedance.com>
2020-04-10 22:08:56 +0800

    TTImagePicker 禁用gif

--- TTBundles/ios_tt_appruntime/TTAppRuntime/Classes/Setting/Feedback/View/View/SSFeedbackPostViewBase.m
+++ TTBundles/ios_tt_appruntime/TTAppRuntime/Classes/Setting/Feedback/View/View/SSFeedbackPostViewBase.m
@@ -598,11 +598,12 @@
 - (TTImagePickerController *)getTTImagePicker {
     if (!_ttImagePickerController) {
         _ttImagePickerController = [[TTImagePickerController alloc] initWithDelegate:self];
+        _ttImagePickerController.enableICloud = YES;
+        _ttImagePickerController.maxResourcesCount = 1;
+        _ttImagePickerController.isGetOriginResource = NO;
+        _ttImagePickerController.isHideGIF = YES;
     }
-    _ttImagePickerController.enableICloud = YES;
-    _ttImagePickerController.maxResourcesCount = 1;
-    _ttImagePickerController.isGetOriginResource = NO;
-    //    _ttImagePickerController.isHideGIF = YES;
+
     return _ttImagePickerController;
 }
 - (void)ttimagePickerController:(TTImagePickerController *)picker didFinishTakePhoto:(UIImage *)photo selectedAssets:(NSArray<TTAsset *> *)assets withInfo:(NSDictionary *)info
