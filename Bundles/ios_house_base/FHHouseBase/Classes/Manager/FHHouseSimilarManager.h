414aefd9f56d35ee9c694cf1576d86fd55d3157f
xiefei.xf <xiefei.xf@bytedance.com>
2020-04-17 19:00:34 +0800

    update:添加相似房源管理类

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Manager/FHHouseSimilarManager.h
@@ -0,0 +1,16 @@
+//
+//  FHHouseSimilarManager.h
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2020/4/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHHouseSimilarManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Manager/FHHouseSimilarManager.m
@@ -0,0 +1,12 @@
+//
+//  FHHouseSimilarManager.m
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2020/4/17.
+//
+
+#import "FHHouseSimilarManager.h"
+
+@implementation FHHouseSimilarManager
+
+@end
