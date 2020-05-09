4c5646931246703ddba19fc10140f9b07b02437b
王志舟 <wangzhizhou@bytedance.com>
2020-05-09 11:48:07 +0800

    解决轮询接口更新未读数时，IM会话数据需要在用户登录后才可以进行

--- Bundles/ios_house_message/FHHouseMessage/Classes/ViewModel/FHMessageViewModel.m
+++ Bundles/ios_house_message/FHHouseMessage/Classes/ViewModel/FHMessageViewModel.m
@@ -129,8 +129,13 @@
 }
 
 - (void)dataLoaded:(FHUnreadMsgModel *)unreadMsg error:(NSError *)error ugcUnread:(FHUnreadMsgDataUnreadModel *)ugcUnread {
-    NSArray<IMConversation *> *allConversations = [[IMManager shareInstance].chatService allConversations];
-    [_combiner resetConversations:allConversations];
+    
+    BOOL isLogin = [TTAccount sharedAccount].isLogin;
+    if(isLogin) {
+        NSArray<IMConversation *> *allConversations = [[IMManager shareInstance].chatService allConversations];
+        [_combiner resetConversations:allConversations];
+    };
+    
     if (self.isFirstLoad) {
         [self.viewController endLoading];
     }
