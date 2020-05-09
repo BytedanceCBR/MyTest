ef39c37931151b43e22e5a80bc376dc992a0b20c
xiesiming <xiesiming@bytedance.com>
2020-05-09 14:01:12 +0800

    重构

---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Manager/FHUGCFeedDetailJumpManager.h
@@ -0,0 +1,16 @@
+//
+//  FHUGCFeedDetailJumpManager.h
+//  FHHouseUGC
+//
+//  Created by 谢思铭 on 2020/5/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHUGCFeedDetailJumpManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Manager/FHUGCFeedDetailJumpManager.m
@@ -0,0 +1,12 @@
+//
+//  FHUGCFeedDetailJumpManager.m
+//  FHHouseUGC
+//
+//  Created by 谢思铭 on 2020/5/9.
+//
+
+#import "FHUGCFeedDetailJumpManager.h"
+
+@implementation FHUGCFeedDetailJumpManager
+
+@end
