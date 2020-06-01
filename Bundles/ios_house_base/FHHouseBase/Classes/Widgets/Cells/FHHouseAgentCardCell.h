48b9a2b0bfd50fbff2e2cbf81f3f49989734789d
xiefei.xf <xiefei.xf@bytedance.com>
2020-06-01 17:21:58 +0800

    update:增加经纪人cell

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Widgets/Cells/FHHouseAgentCardCell.h
@@ -0,0 +1,16 @@
+//
+//  FHHouseAgentCardCell.h
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2020/6/1.
+//
+
+#import "FHListBaseCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHHouseAgentCardCell : FHListBaseCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Widgets/Cells/FHHouseAgentCardCell.m
@@ -0,0 +1,23 @@
+//
+//  FHHouseAgentCardCell.m
+//  AKCommentPlugin
+//
+//  Created by 谢飞 on 2020/6/1.
+//
+
+#import "FHHouseAgentCardCell.h"
+
+@implementation FHHouseAgentCardCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
