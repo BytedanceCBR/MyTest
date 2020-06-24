7c71f395d895c1929eb201f717be648e6a6e29bb
xiesiming <xiesiming@bytedance.com>
2020-06-24 17:51:29 +0800

    解决cr问题

--- Article/News/AppDelegate.m
+++ Article/News/AppDelegate.m
@@ -22,7 +22,6 @@
 
 extern NSString * const SSCommonLogicSettingWebViewQueryStringEnableKey;
 extern NSString * const SSCommonLogicSettingWebViewQueryStringListKey;
-CFAbsoluteTime mainStartTime;
 
 const char * build_rev() ;
 
@@ -98,7 +97,6 @@ extern NSString *const kTTAppseeEnableKey;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     /// 注意，这里必须初始化这个Manager.勿删
-    mainStartTime = CFAbsoluteTimeGetCurrent();
     [self initWebViewCommonQueryStatus];
     [[TTLaunchTracer shareInstance] setLaunchFrom:TTAPPLaunchFromInitialState];
 
