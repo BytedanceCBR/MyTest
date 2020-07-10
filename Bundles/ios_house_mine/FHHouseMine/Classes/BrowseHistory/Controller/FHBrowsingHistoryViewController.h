2711abbc8628e2dc574549634e7af923aaa2a635
wangxinyu <wangxinyu.kevin@bytedance.com>
2020-07-10 18:35:20 +0800

    [new]: 浏览历史需求

---
+++ Bundles/ios_house_mine/FHHouseMine/Classes/BrowseHistory/Controller/FHBrowsingHistoryViewController.h
@@ -0,0 +1,17 @@
+//
+//  FHBrowsingHistoryViewController.h
+//  BDSSOAuthSDK-BDSSOAuthSDK
+//
+//  Created by wangxinyu on 2020/7/10.
+//
+
+#import <UIKit/UIKit.h>
+#import "FHBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHBrowsingHistoryViewController : FHBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_mine/FHHouseMine/Classes/BrowseHistory/Controller/FHBrowsingHistoryViewController.m
@@ -0,0 +1,29 @@
+//
+//  FHBrowsingHistoryViewController.m
+//  BDSSOAuthSDK-BDSSOAuthSDK
+//
+//  Created by wangxinyu on 2020/7/10.
+//
+
+#import "FHBrowsingHistoryViewController.h"
+
+@interface FHBrowsingHistoryViewController ()
+
+@end
+
+@implementation FHBrowsingHistoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (instancetype)initWithRouteParamObj:(TTRouteParamObj *)paramObj {
+    self = [super initWithRouteParamObj:paramObj];
+    if (self) {
+        //TODO: 初始化
+    }
+    return self;
+}
+
+@end
