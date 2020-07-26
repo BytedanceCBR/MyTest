8f9474cb14bad3a4422c26fd7db061241024a28a
wangxinyu <wangxinyu.kevin@bytedance.com>
2020-07-26 21:06:45 +0800

    index保护

--- Bundles/ios_house_find/FHHouseFind/Classes/HelpFind/ViewModel/FHHouseFindHelpViewModel.m
+++ Bundles/ios_house_find/FHHouseFind/Classes/HelpFind/ViewModel/FHHouseFindHelpViewModel.m
@@ -398,13 +398,18 @@ extern NSString *const kFHPLoginhoneNumberCacheKey;
     } else {
         // Fallback on earlier versions
     }
-    __block NSInteger regionIndex = 0;
+    
+    __block NSInteger regionIndex = -1;
     [model.items enumerateObjectsUsingBlock:^(FHHouseFindSelectItemModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         if (obj.tabId == FHSearchTabIdTypeRegion) {
             regionIndex = idx;
             *stop = YES;
         }
     }];
+    if (regionIndex == -1) {
+        return;
+    }
+    
     NSArray<FHFilterNodeModel*> *configs = [self.class convertConfigItemsToModel:@[model.items[regionIndex].configOption]];
     __weak typeof(self)wself = self;
     frame.size.height = REGION_CONTENT_HEIGHT + bottomHeight;
