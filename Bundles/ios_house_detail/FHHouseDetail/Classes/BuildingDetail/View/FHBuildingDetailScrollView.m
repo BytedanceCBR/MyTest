b962d1d4e8ece488655bf6cc60f1883f6d07bb32
luowentao <luowentao@bytedance.com>
2020-08-03 15:30:38 +0800

    楼栋详情页

--- Bundles/ios_house_detail/FHHouseDetail/Classes/BuildingDetail/View/FHBuildingDetailScrollView.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/BuildingDetail/View/FHBuildingDetailScrollView.m
@@ -7,14 +7,42 @@
 
 #import "FHBuildingDetailScrollView.h"
 
+@interface FHBuildingDetailScrollView() <UIGestureRecognizerDelegate>
+
+@end
+
 @implementation FHBuildingDetailScrollView
 
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event     {
-         if (point.x < 10) {
-             return nil;
-         } else {
-             return [super hitTest:point withEvent:event];
-         }
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    if ([self panBack:gestureRecognizer]) {
+        return YES;
+    }
+    return NO;
 }
 
+- (BOOL)panBack:(UIGestureRecognizer *)gestureRecognizer {
+    CGFloat location_X = 0.15 * [UIScreen mainScreen].bounds.size.width;                                     //屏幕设置的阈值
+    if (gestureRecognizer == self.panGestureRecognizer) {
+        UIPanGestureRecognizer *pan = (UIPanGestureRecognizer *)gestureRecognizer;
+        CGPoint point = [pan translationInView:self];                                                        //获取到的是手指移动后，在相对坐标中的偏移量（也就是这个移动最终是向前还是向后）
+       // NSLog(@"luowentao 获取到的是手指移动后，在相对坐标中的偏移量 point = %@",NSStringFromCGPoint(point));
+        UIGestureRecognizerState state = gestureRecognizer.state;
+        if (UIGestureRecognizerStateBegan == state || UIGestureRecognizerStatePossible == state) {
+            CGPoint location = [gestureRecognizer locationInView:self];                                     //获取到的是手指点击屏幕实时的坐标点
+          //  NSLog(@"luowentao 获取到的是手指点击屏幕实时的坐标点 location = %@",NSStringFromCGPoint(location));
+            if (point.x >0 && location.x < location_X) {
+                return YES;
+            }
+        }
+        
+    }
+    return NO;
+}
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    if ([self panBack:gestureRecognizer]) {
+        return NO;
+    }
+    return YES;
+}
 @end
