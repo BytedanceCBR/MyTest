6875558164a0c35ae2dbd947241bd734d8b5daa0
xubinbin.19971226 <xubinbin.19971226@bytedance.com>
2020-08-18 11:29:38 +0800

    fix

--- Bundles/ios_house_message/FHHouseMessage/Classes/ViewModel/FHConversationDataCombiner.m
+++ Bundles/ios_house_message/FHHouseMessage/Classes/ViewModel/FHConversationDataCombiner.m
@@ -90,11 +90,17 @@ static FHCombineChannelSingleton* _instance;
     if(self.channels.count == 0 && [FHCombineChannelSingleton shareInstance].combineChannels.count > 0) {
         self.channels = [FHCombineChannelSingleton shareInstance].combineChannels;
     }
-    [theItems addObjectsFromArray:_channels];
+    self.items = [self sortItems:theItems];
+    self.conversations = [self sortItems:self.conversations];
+    self.channels = [self sortItems:self.channels];
+}
+
+- (NSArray *)sortItems:(NSArray *)items {
+    NSMutableArray<id<ConversationComparable>>* theItems = [NSMutableArray arrayWithArray:items];
     NSArray* result = [theItems sortedArrayUsingComparator:^NSComparisonResult(id<ConversationComparable>  _Nonnull obj1, id<ConversationComparable>  _Nonnull obj2) {
         return [FHConversationDataCombiner conversationComparison:obj1 toOther:obj2];
     }];
-    self.items = result;
+    return result;
 }
 
 -(NSInteger)numberOfItems {
