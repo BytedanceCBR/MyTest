01f99a47aa75dce8892379524663cf4e0132cb48
liuyu.shelley <liuyu.shelley@bytedance.com>
2020-08-24 21:40:18 +0800

    [update]增加宏开关

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Utils/FHErrorHubHelper.h
@@ -0,0 +1,13 @@
+//
+//  FHErrorHubHelper.h
+//  FHHouseBase
+//
+//  Created by liuyu on 2020/8/24.
+//
+
+#ifndef FHErrorHubHelper_h
+#define FHErrorHubHelper_h
+
+#define ISOPENLOCALTESTCHECK 0
+
+#endif /* FHErrorHubHelper_h */
--- Bundles/ios_house_base/FHHouseBase/Classes/Utils/FHErrorHubManagerUtil.m
+++ Bundles/ios_house_base/FHHouseBase/Classes/Utils/FHErrorHubManagerUtil.m
@@ -6,32 +6,44 @@
 //
 
 #import "FHErrorHubManagerUtil.h"
+#import "FHErrorHubHelper.h"
 #import "TTSandBoxHelper.h"
 
 @implementation FHErrorHubManagerUtil
 + (void)checkRequestResponseWithHost:(NSString *)host requestParams:(NSDictionary *)params responseStatus:(TTHttpResponse *)responseStatus response:(id)response analysisError:(NSError *)analysisError changeModelType:(FHNetworkMonitorType )type {
+    
+    if (ISOPENLOCALTESTCHECK && ![self canUseDefine]) {
+        [self openErrorHubSwitch];
+    }
+    
     if (![[self getChannel] isEqualToString:@"local_test"] || ![self errorHubSwitch]) {
         return;
     }
+    
     Class cls = NSClassFromString(@"FHHouseErrorHubManager");
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored   "-Wundeclared-selector"
-     NSObject <FHErrorHubManagerProtocol>*errorhub = [cls performSelector:@selector(sharedInstance)];
-           [errorhub checkRequestResponseWithHost:host requestParams:params responseStatus:responseStatus response:response analysisError:analysisError changeModelType:type errorHubType:1];
-    #pragma clang diagnostic pop
-   
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored   "-Wundeclared-selector"
+    NSObject <FHErrorHubManagerProtocol>*errorhub = [cls performSelector:@selector(sharedInstance)];
+    [errorhub checkRequestResponseWithHost:host requestParams:params responseStatus:responseStatus response:response analysisError:analysisError changeModelType:type errorHubType:1];
+#pragma clang diagnostic pop
+    
 }
 
 + (void)checkBuryingPointWithEvent:(NSString *)eventName Params:(NSDictionary* )eventParams {
+    
+    if (ISOPENLOCALTESTCHECK && ![self canUseDefine] ) {
+        [self openErrorHubSwitch];
+    }
+    
     if (![[self getChannel] isEqualToString:@"local_test"] || ![self errorHubSwitch]) {
         return;
     }
     Class cls = NSClassFromString(@"FHHouseErrorHubManager");
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored   "-Wundeclared-selector"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored   "-Wundeclared-selector"
     NSObject <FHErrorHubManagerProtocol>*errorhub = [cls performSelector:@selector(sharedInstance)];
     [errorhub checkBuryingPointWithEvent:eventName Params:eventParams];
-    #pragma clang diagnostic pop
+#pragma clang diagnostic pop
 }
 
 + (NSString*)getChannel {
@@ -42,4 +54,14 @@
 + (BOOL)errorHubSwitch {
     return [[NSUserDefaults standardUserDefaults] boolForKey:@"_errorHubSwitch"];
 }
+
++ (BOOL)canUseDefine {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:@"_dontUseDefine"];
+}
+
++ (void)openErrorHubSwitch {
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"_errorHubSwitch"];
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"_dontUseDefine"];
+}
+
 @end
