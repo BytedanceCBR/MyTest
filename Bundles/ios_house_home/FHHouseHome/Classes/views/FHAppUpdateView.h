8b076e6706410edb942581bb60038af52fb17c65
xielei.233 <xielei.233@bytedance.com>
2020-09-21 22:07:13 +0800

    升级SDK更新

---
+++ Bundles/ios_house_home/FHHouseHome/Classes/views/FHAppUpdateView.h
@@ -0,0 +1,16 @@
+//
+//  FHAppUpdateView.h
+//  FHHouseHome
+//
+//  Created by bytedance on 2020/9/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHAppUpdateView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_home/FHHouseHome/Classes/views/FHAppUpdateView.m
@@ -0,0 +1,20 @@
+//
+//  FHAppUpdateView.m
+//  FHHouseHome
+//
+//  Created by bytedance on 2020/9/21.
+//
+
+#import "FHAppUpdateView.h"
+
+@implementation FHAppUpdateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
