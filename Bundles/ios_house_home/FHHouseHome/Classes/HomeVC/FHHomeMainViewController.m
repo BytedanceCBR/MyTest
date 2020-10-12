124ea5c0777a2d4023cc3c1153f57a73ce418889
wangzhizhou <wangzhizhou@bytedance.com>
2020-10-12 22:12:39 +0800

    bugfix: 运营弹窗和升级弹窗重叠问题修复

--- Bundles/ios_house_home/FHHouseHome/Classes/HomeVC/FHHomeMainViewController.m
+++ Bundles/ios_house_home/FHHouseHome/Classes/HomeVC/FHHomeMainViewController.m
@@ -54,9 +54,7 @@ static NSString * const kFUGCPrefixStr = @"fugc";
     if (self = [super init]) {
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [self checkLocalTestUpgradeVersionAlert];
-            });
+            [self checkLocalTestUpgradeVersionAlert];
         });
     }
     return self;
