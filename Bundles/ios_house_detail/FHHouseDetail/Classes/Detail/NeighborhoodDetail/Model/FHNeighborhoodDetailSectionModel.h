22caef2309b962b223e0a35c263831e369cef8a8
luowentao <luowentao@bytedance.com>
2020-10-15 16:56:53 +0800

    新版小区详情页框架 顶部导航

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/NeighborhoodDetail/Model/FHNeighborhoodDetailSectionModel.h
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/NeighborhoodDetail/Model/FHNeighborhoodDetailSectionModel.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 typedef NS_ENUM(NSUInteger, FHNeighborhoodDetailSectionType) {
-    FHNeighborhoodDetailSectionTypeHeader,
+    FHNeighborhoodDetailSectionTypeHeader = 10086,
     FHNeighborhoodDetailSectionTypeBaseInfo,
     FHNeighborhoodDetailSectionTypeFloorpan,
     FHNeighborhoodDetailSectionTypeHouseSale,
