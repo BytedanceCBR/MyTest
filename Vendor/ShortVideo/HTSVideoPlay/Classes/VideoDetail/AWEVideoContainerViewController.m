5dee1e1e173daa523aa014461617f736dd3c4e18
liuyu.shelley <liuyu.shelley@bytedance.com>
2020-10-15 11:06:33 +0800

    [update]小视频提交

--- Vendor/ShortVideo/HTSVideoPlay/Classes/VideoDetail/AWEVideoContainerViewController.m
+++ Vendor/ShortVideo/HTSVideoPlay/Classes/VideoDetail/AWEVideoContainerViewController.m
@@ -898,7 +898,7 @@ const static CGFloat kAWEVideoContainerSpacing = 2;
                     [self sendVideoOverTracking];
                     [self sendStayPageTracking];
                     self.firstPageShown = YES;
-
+                    self.currentVideoCell = nil;
                     self.dataFetchManager.currentIndex = itemIndex;
                     self.currentVideoCell = cell;
                     self.currentIndexPath = newIndexPath;
