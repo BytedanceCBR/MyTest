b308b38edb8d733d02e7cb97a6d683f75da4cf43
bytedance <zhuzhiwen@bytedance.com>
2020-10-29 20:14:05 +0800

    新房大类页icon打点

--- Bundles/ios_house_list/FHHouseList/Classes/MainPage/View/New/Entrances/ViewModel/FHHouseNewEntrancesViewModel.m
+++ Bundles/ios_house_list/FHHouseList/Classes/MainPage/View/New/Entrances/ViewModel/FHHouseNewEntrancesViewModel.m
@@ -7,6 +7,11 @@
 
 #import "FHHouseNewEntrancesViewModel.h"
 #import "FHEnvContext.h"
+#import "TTRoute.h"
+#import "FHUserTracker.h"
+#import "FHCommuteManager.h"
+#import "NSObject+FHTracker.h"
+#import "NSDictionary+BTDAdditions.h"
 
 @interface FHHouseNewEntrancesViewModel()
 @property (nonatomic, strong, readwrite) NSArray *items;
@@ -17,7 +22,7 @@
 - (NSArray *)items {
     if (!_items) {
         FHConfigDataModel *configModel = [[FHEnvContext sharedInstance] getConfigFromCache];
-        NSArray *items = configModel.houseOpData2.items;
+        NSArray *items = configModel.courtOpData.items;
         if (items.count > 5) {
             items = [items subarrayWithRange:NSMakeRange(0, 5 )];
         }
@@ -33,7 +38,45 @@
 }
 
 - (void)onClickItem:(FHConfigDataOpDataItemsModel *)item {
+    NSString *openUrl = item.openUrl;
+    if (!openUrl || !openUrl.length || ![openUrl isKindOfClass:[NSString class]]) return;
+
+    NSDictionary *logPbParams = [FHTracerModel getLogPbParams:item.logPb];
+    NSString *originFrom = [logPbParams btd_stringValueForKey:UT_ORIGIN_FROM];
+    if (!originFrom) {
+        originFrom = self.fh_trackModel.originFrom;
+    }
+    NSString *elementFrom = [logPbParams btd_stringValueForKey:@"icon_name"];
+    if (!elementFrom) {
+        elementFrom = self.fh_trackModel.elementFrom;
+    }
     
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    dict[UT_ENTER_FROM] = self.fh_trackModel.pageType ? : @"be_null";
+    dict[UT_ORIGIN_FROM] = originFrom ? : @"be_null";
+    dict[UT_ELEMENT_FROM] = elementFrom ? : @"be_null";
+    dict[UT_LOG_PB] = logPbParams ? : @"be_null";
+
+    NSURL *url = [NSURL URLWithString:openUrl];
+    if ([openUrl containsString:@"://commute_list"]){
+        //通勤找房
+        [[FHCommuteManager sharedInstance] tryEnterCommutePage:openUrl logParam:dict];
+    } else {
+        NSDictionary *userInfoDict = @{@"tracer":dict};
+        TTRouteUserInfo *userInfo = [[TTRouteUserInfo alloc] initWithInfo:userInfoDict];
+        [[TTRoute sharedRoute] openURLByPushViewController:url userInfo:userInfo];
+    }
+    
+    [self trackerClickIcon:logPbParams];
 }
 
+- (void)trackerClickIcon:(NSDictionary *)logPbParams {
+    NSMutableDictionary *logParams = [NSMutableDictionary dictionary];
+    if (logPbParams) {
+        [logParams addEntriesFromDictionary:logPbParams];
+    }
+    
+    logParams[UT_PAGE_TYPE] = self.fh_trackModel.pageType ? : @"be_null";
+    [FHUserTracker writeEvent:@"click_icon" params:logParams];
+}
 @end
