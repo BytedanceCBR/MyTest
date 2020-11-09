224661627e59db171d0312159b00de76a4b2b4c6
liuyu.shelley <liuyu.shelley@bytedance.com>
2020-11-09 18:27:09 +0800

    [update]登陆弹窗修改

--- Vendor/ShortVideo/HTSVideoPlay/Classes/VideoDetail/AWEVideoDetailViewController.m
+++ Vendor/ShortVideo/HTSVideoPlay/Classes/VideoDetail/AWEVideoDetailViewController.m
@@ -1956,6 +1956,7 @@ static const CGFloat kFloatingViewOriginY = 230;
         [params setObject:@"click_publisher" forKey:@"enter_type"];
         // 登录成功之后不自己Pop，先进行页面跳转逻辑，再pop
         [params setObject:@(YES) forKey:@"need_pop_vc"];
+        params[@"from_ugc"] = @(YES);
         [TTAccountLoginManager showAlertFLoginVCWithParams:params completeBlock:^(TTAccountAlertCompletionEventType type, NSString * _Nullable phoneNum) {
             if (type == TTAccountAlertCompletionEventTypeDone) {
                 //登录成功 走发送逻辑
