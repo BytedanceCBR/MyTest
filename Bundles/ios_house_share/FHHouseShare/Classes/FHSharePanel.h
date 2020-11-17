806febd4680e43f347a4463e24b604facd456197
molijie <molijie@bytedance.com>
2020-11-17 19:13:49 +0800

    [update] 新建FHHouseShared的developmentPod

---
+++ Bundles/ios_house_share/FHHouseShare/Classes/FHSharePanel.h
@@ -0,0 +1,19 @@
+//
+//  FHSharePanel.h
+//  FHHouseShare
+//
+//  Created by bytedance on 2020/11/3.
+//
+
+#import <Foundation/Foundation.h>
+#import <BDUGActivityPanelControllerProtocol.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHSharePanel : NSObject <BDUGActivityPanelControllerProtocol>
+
+@property (nonatomic, weak, nullable) id<BDUGActivityPanelDelegate> delegate;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_share/FHHouseShare/Classes/FHShareWeChatTask.m
@@ -0,0 +1,38 @@
+//
+//  FHShareWeChatTask.m
+//  FHHouseShare
+//
+//  Created by bytedance on 2020/11/4.
+//
+
+#import "FHShareWeChatTask.h"
+#import <TTLaunchDefine.h>
+#import <BDUGWeChatShare.h>
+#import <NewsBaseDelegate.h>
+
+DEC_TASK("FHShareWeChatTask",FHTaskTypeSDKs,TASK_PRIORITY_HIGH);
+
+@implementation FHShareWeChatTask
+
+-(NSString *)taskIdentifier {
+    return @"FHShareWeChatTask";
+}
+
+-(BOOL)isResident {
+    return YES;
+}
+
+-(void)startWithApplication:(UIApplication *)application options:(NSDictionary *)launchOptions {
+    [BDUGWeChatShare sharedWeChatShare].delegate =(id <BDUGWechatShareDelegate>) [UIApplication sharedApplication].delegate;
+    [BDUGWeChatShare registerWithID:[SharedAppDelegate weixinAppID] universalLink:@"https://i.haoduofangs.com/"];
+}
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [BDUGWeChatShare handleOpenURL:url];
+}
+
+-(BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+    return [BDUGWeChatShare handleOpenUniversalLink:userActivity];
+}
+
+@end
