58d0cff5192316c36a16e57555eea4cafcd7b494
上官祥正 <shangguanxiangzheng@bytedance.com>
2020-11-27 14:39:10 +0800

    sugwordshow埋点问题

--- Bundles/ios_house_list/FHHouseList/Classes/SuggestionList/ViewController/FHSuggestionListViewController+FHTracker.m
+++ Bundles/ios_house_list/FHHouseList/Classes/SuggestionList/ViewController/FHSuggestionListViewController+FHTracker.m
@@ -82,14 +82,14 @@ static const char tabSwitchedKey;
             FHSuggestionResponseItemModel *item = (FHSuggestionResponseItemModel *)object;
             return item.cardType == 16;
         }]];
-        
         count = filteredResultArray.count + filteredOtherResultArray.count;
-        tagsStr = [self resultTagsString:filteredResultArray houseType:houseType];
-        
     }
     NSMutableArray *itemArray = [[NSMutableArray alloc] init];
     [itemArray addObjectsFromArray:result.data.items];
     [itemArray addObjectsFromArray:result.data.otherItems];
+    if(count){
+        tagsStr = [self resultTagsString:itemArray houseType:houseType];
+    }
     NSMutableDictionary *differResultnum = [NSMutableDictionary new];
     NSInteger newNum = [self getDifferResultnum:itemArray houseType:FHHouseTypeNewHouse];
     NSInteger oldNum = [self getDifferResultnum:itemArray houseType:FHHouseTypeSecondHandHouse];
@@ -138,6 +138,9 @@ static const char tabSwitchedKey;
     [tagStr appendString:@"{"];
     BOOL firstTag = YES;
     for (FHSuggestionResponseItemModel *item in resultArray) {
+        if(item.cardType != 16){
+            continue;
+        }
         if (!firstTag) {
             [tagStr appendFormat:@","];
         }
@@ -147,7 +150,7 @@ static const char tabSwitchedKey;
         if (!name || !name.length) continue;;
         
         firstTag = NO;
-        if (item.newtip && item.newtip.content.length && houseType == FHHouseTypeSecondHandHouse) {
+        if (item.newtip && item.newtip.content.length &&[item.houseType intValue]== FHHouseTypeSecondHandHouse) {
             [tagStr appendFormat:@"%@|%@", item.newtip.content, name];
         } else {
             [tagStr appendFormat:@"%@", name];
