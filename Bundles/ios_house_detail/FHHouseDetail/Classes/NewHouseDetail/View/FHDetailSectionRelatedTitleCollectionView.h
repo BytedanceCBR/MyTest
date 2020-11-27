f8fb3f071c91fc14de04b5df9fc8b09782d28901
xubinbin.19971226 <xubinbin.19971226@bytedance.com>
2020-11-27 18:06:57 +0800

    fix

--- Bundles/ios_house_detail/FHHouseDetail/Classes/NewHouseDetail/View/FHDetailSectionRelatedTitleCollectionView.h
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/NewHouseDetail/View/FHDetailSectionRelatedTitleCollectionView.h
@@ -5,12 +5,21 @@
 //  Created by xubinbin on 2020/11/27.
 //
 
-#import "FHDetailSectionTitleCollectionView.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FHDetailSectionRelatedTitleCollectionView : FHDetailSectionTitleCollectionView
+@interface FHDetailSectionRelatedTitleCollectionView : UICollectionReusableView
 
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@property (nonatomic, strong) UILabel *subTitleLabel;
+
+@property (nonatomic, strong) UIImageView *arrowsImg;
+
+@property (nonatomic, copy) void (^moreActionBlock)(void);
+
+- (void)setSubTitleWithTitle:(NSString *)subTitle;
 @end
 
 NS_ASSUME_NONNULL_END
--- Bundles/ios_house_detail/FHHouseDetail/Classes/NewHouseDetail/View/FHDetailSectionRelatedTitleCollectionView.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/NewHouseDetail/View/FHDetailSectionRelatedTitleCollectionView.m
@@ -12,26 +12,55 @@
 
 @implementation FHDetailSectionRelatedTitleCollectionView
 
-- (void)prepareForReuse {
-    [super prepareForReuse];
-    self.arrowsImg.hidden = YES;
-    self.subTitleLabel.hidden = YES;
-    [self.titleLabel mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(15);
-        make.top.mas_equalTo(20);
-    }];
-}
-
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        [self.titleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        self.titleLabel = [[UILabel alloc] init];
+        self.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        self.titleLabel.font = [UIFont themeFontRegular:14];
+        self.titleLabel.textColor = [UIColor colorWithHexStr:@"#6d7278"];
+        [self addSubview:self.titleLabel];
+        [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(15);
             make.top.mas_equalTo(20);
         }];
         
+        self.arrowsImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"arrowicon-feed-4"]];
+        self.arrowsImg.hidden = YES;
+        [self addSubview:self.arrowsImg];
+        [self.arrowsImg mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.right.mas_equalTo(self).offset(-12);
+            make.height.width.mas_equalTo(20);
+            make.centerY.mas_equalTo(self.titleLabel);
+        }];
+        
+        self.subTitleLabel = [[UILabel alloc] init];
+        self.subTitleLabel.font = [UIFont themeFontRegular:14];
+        self.subTitleLabel.textColor = [UIColor themeGray2];
+        self.subTitleLabel.hidden = YES;
+        [self addSubview:self.subTitleLabel];
+        [self.subTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(self.titleLabel.mas_right).mas_offset(6);
+            make.centerY.mas_equalTo(self.titleLabel);
+        }];
+        
+        [self addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(moreAction:)]];
     }
     return self;
 }
 
+- (void)setSubTitleWithTitle:(NSString *)subTitle{ //一定要先设置Label的内容再设置
+    if (subTitle.length > 0) {
+        self.subTitleLabel.text = [NSString stringWithFormat:@"| %@",subTitle];
+        self.subTitleLabel.hidden = NO;
+    } else {
+        self.subTitleLabel.hidden = YES;
+    }
+}
+
+- (void)moreAction:(UITapGestureRecognizer *)tapGesture {
+    if (self.moreActionBlock) {
+        self.moreActionBlock();
+    }
+}
 
 @end
