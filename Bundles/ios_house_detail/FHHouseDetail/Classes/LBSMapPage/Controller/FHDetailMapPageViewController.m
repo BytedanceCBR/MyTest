7268278c3cfcd895c0ac5ae59c0787e906a34d34
xielei.233 <xielei.233@bytedance.com>
2020-12-09 19:11:30 +0800

    地图样式优化

--- Bundles/ios_house_detail/FHHouseDetail/Classes/LBSMapPage/Controller/FHDetailMapPageViewController.m
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/LBSMapPage/Controller/FHDetailMapPageViewController.m
@@ -605,15 +605,15 @@ static MAMapView *kFHPageMapView = nil;
         kFHPageMapView = [[MAMapView alloc] initWithFrame:mapFrame];// 不会同时出两个页面
 
         //设置地图style
-        NSString *stylePath = [[NSBundle mainBundle] pathForResource:@"gaode_house_detail_style.data" ofType:nil];
-        NSData *data = [NSData dataWithContentsOfFile:stylePath];
-        NSString *extraPath = [[NSBundle mainBundle] pathForResource:@"gaode_house_detail_style_extra.data" ofType:nil];
-        NSData *extraData = [NSData dataWithContentsOfFile:extraPath];
-        MAMapCustomStyleOptions *options = [MAMapCustomStyleOptions new];
-        options.styleData = data;
-        options.styleExtraData = extraData;
-        [kFHPageMapView setCustomMapStyleOptions:options];
-        [kFHPageMapView setCustomMapStyleEnabled:YES];
+//        NSString *stylePath = [[NSBundle mainBundle] pathForResource:@"gaode_house_detail_style.data" ofType:nil];
+//        NSData *data = [NSData dataWithContentsOfFile:stylePath];
+//        NSString *extraPath = [[NSBundle mainBundle] pathForResource:@"gaode_house_detail_style_extra.data" ofType:nil];
+//        NSData *extraData = [NSData dataWithContentsOfFile:extraPath];
+//        MAMapCustomStyleOptions *options = [MAMapCustomStyleOptions new];
+//        options.styleData = data;
+//        options.styleExtraData = extraData;
+//        [kFHPageMapView setCustomMapStyleOptions:options];
+//        [kFHPageMapView setCustomMapStyleEnabled:YES];
 
         kFHPageMapView.zoomLevel  = 15.5;
         [kFHPageMapView setCenterCoordinate:self.centerPoint];
