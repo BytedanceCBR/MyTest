4207346c2dd7af811b785faebf12ed2465da729b
liuyu.shelley <liuyu.shelley@bytedance.com>
2020-12-09 11:11:54 +0800

    [update]小区评测修改

--- Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/Model/FHDetailNeighborhoodModel.h
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/Detail/Model/FHDetailNeighborhoodModel.h
@@ -21,6 +21,15 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy , nullable) NSString *title;
 @end
 
+@protocol FHDetailNeighborhoodDataNeighborhoodEvaluationScoreModel<NSObject>
+@end
+
+@interface FHDetailNeighborhoodDataNeighborhoodEvaluationScoreModel : JSONModel
+
+@property (nonatomic, copy , nullable) NSString *value;
+@property (nonatomic, copy , nullable) NSString *schema;
+@end
+
 @interface FHDetailNeighborhoodDataNeighborhoodEvaluationModel : JSONModel
 
 @property (nonatomic, strong , nullable) NSArray<FHDetailNeighborhoodDataNeighborhoodEvaluationEvaluationListModel> *evaluationList;
@@ -39,6 +48,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy , nullable) NSString *groupId;
 @property (nonatomic, copy , nullable) NSString *schema;
 @property (nonatomic, strong , nullable) NSDictionary *logPb;
+@property (nonatomic, strong , nullable) FHDetailNeighborhoodDataNeighborhoodEvaluationScoreModel *score;
+@property (nonatomic, strong , nullable) NSDictionary *compare;
 @property (nonatomic, copy , nullable) NSString *iconUrl;
 @property (nonatomic, assign) BOOL hiddenBottomLine;
 @end
@@ -46,6 +57,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface FHDetailNeighborhoodDataStrategyModel : JSONModel
 
 @property (nonatomic, strong , nullable) NSArray<FHDetailNeighborhoodDataStrategyArticleListModel> *articleList;
+@property (nonatomic, strong , nullable) FHDetailNeighborhoodDataStrategyArticleListModel *article;
 @property (nonatomic, copy , nullable) NSString *title;
 @end
 
