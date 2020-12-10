0650b51f8f9f601efe73247888bd24b58698f478
上官祥正 <shangguanxiangzheng@bytedance.com>
2020-12-10 21:13:43 +0800

    ui布局

---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/NeighborhoodDetail/View/FHNeighborhoodDetailTradingRecordCell.h
@@ -0,0 +1,16 @@
+//
+//  FHNeighborhoodDetailTradingRecordCell.h
+//  FHHouseDetail
+//
+//  Created by bytedance on 2020/12/10.
+//
+
+#import "FHDetailBaseCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHNeighborhoodDetailTradingRecordCell : FHDetailBaseCollectionCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_detail/FHHouseDetail/Classes/NeighborhoodDetail/View/FHNeighborhoodDetailTradingRecordCell.m
@@ -0,0 +1,12 @@
+//
+//  FHNeighborhoodDetailTradingRecordCell.m
+//  FHHouseDetail
+//
+//  Created by bytedance on 2020/12/10.
+//
+
+#import "FHNeighborhoodDetailTradingRecordCell.h"
+
+@implementation FHNeighborhoodDetailTradingRecordCell
+
+@end
