c467987869ee598845eb124472ee4fb10ebaa0a9
上官祥正 <shangguanxiangzheng@bytedance.com>
2021-01-06 14:52:32 +0800

    新建类

---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Manager/FHOneTouchBackManager.h
@@ -0,0 +1,16 @@
+//
+//  FHOneTouchBackManager.h
+//  FHHouseBase
+//
+//  Created by bytedance on 2021/1/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FHOneTouchBackManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bundles/ios_house_base/FHHouseBase/Classes/Manager/FHOneTouchBackManager.m
@@ -0,0 +1,12 @@
+//
+//  FHOneTouchBackManager.m
+//  FHHouseBase
+//
+//  Created by bytedance on 2021/1/6.
+//
+
+#import "FHOneTouchBackManager.h"
+
+@implementation FHOneTouchBackManager
+
+@end
