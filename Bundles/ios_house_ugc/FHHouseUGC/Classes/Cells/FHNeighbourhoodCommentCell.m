157e3e5a717aea72cab7343e57d64cdbbd91b1ec
xubinbin.19971226 <xubinbin.19971226@bytedance.com>
2021-01-11 22:19:54 +0800

    二手房详情页小区点评精华标签修复

--- Bundles/ios_house_ugc/FHHouseUGC/Classes/Cells/FHNeighbourhoodCommentCell.m
+++ Bundles/ios_house_ugc/FHHouseUGC/Classes/Cells/FHNeighbourhoodCommentCell.m
@@ -192,7 +192,8 @@
 //    [self.userInfoView.icon bd_setImageWithURL:[NSURL URLWithString:self.cellModel.user.avatarUrl] placeholder:[UIImage imageNamed:@"fh_mine_avatar"]];
     
     [self.userInfoView refreshWithData:self.cellModel];
-    self.userInfoView.essenceIcon.hidden = YES;
+    [self.userInfoView showEssenceIcon];
+    //self.userInfoView.essenceIcon.hidden = YES;
     
     self.userInfoView.moreBtn.hidden = YES;
     
@@ -229,13 +230,13 @@
     //图片
     [self.multiImageView updateImageView:self.cellModel.imageList largeImageList:self.cellModel.largeImageList];
     
-    if(self.cellModel.isStick && self.cellModel.stickStyle == FHFeedContentStickStyleGood) {
-        self.decorationImageView.width = 42;
-        self.decorationImageView.height = 42;
-        self.decorationImageView.left = cellWidth - rightMargin - rightPadding - 42;
-        self.decorationImageView.top = self.contentContainer.top + topPadding - 1;
-        [self.decorationImageView setImage:[UIImage imageNamed:@"fh_ugc_wenda_essence_small"]];
-    }
+//    if(self.cellModel.isStick && self.cellModel.stickStyle == FHFeedContentStickStyleGood) {
+//        self.decorationImageView.width = 42;
+//        self.decorationImageView.height = 42;
+//        self.decorationImageView.left = cellWidth - rightMargin - rightPadding - 42;
+//        self.decorationImageView.top = self.contentContainer.top + topPadding - 1;
+//        [self.decorationImageView setImage:[UIImage imageNamed:@"fh_ugc_wenda_essence_small"]];
+//    }
 }
 
 - (void)deleteCell {
