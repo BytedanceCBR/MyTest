28e80b98803942ebb750c2211aec2f6917e629d1
陈保瑞 <chenbaorui.gxl@bytedance.com>
2021-02-01 14:10:17 +0800

    Initial Commit

---
+++ MyCollectionViewTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyCollectionViewTest
+//
+//  Created by bytedance on 2021/2/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MyCollectionViewTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MyCollectionViewTest
+//
+//  Created by bytedance on 2021/2/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
