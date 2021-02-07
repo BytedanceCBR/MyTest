d9dc6f422db52bc660271cb352c653a49013922e
陈保瑞 <chenbaorui.gxl@bytedance.com>
2021-02-07 20:16:29 +0800

    Initial Commit

---
+++ MyCollectionView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MyCollectionView
+//
+//  Created by bytedance on 2021/2/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyCollectionView/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MyCollectionView
+//
+//  Created by bytedance on 2021/2/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
